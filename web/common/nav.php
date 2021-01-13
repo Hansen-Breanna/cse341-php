fd1417ead8382fdcbb0168e3f4b25986929d9f34
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 15:33:43 -0700

    change nav content color

--- web/common/nav.php
+++ web/common/nav.php
@@ -14,17 +14,17 @@
     <header>
         <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-dark border-bottom box-shadow mb-3">
             <div class="container">
-                <a class="navbar-brand text-light">CSE 341 Software Engineering I</a>
+                <a class="navbar-brand text-dark">CSE 341 Software Engineering I</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
                     <ul class="navbar-nav flex-grow-1">
                         <li class="nav-item">
-                            <a class="nav-link text-light" href="index.php">Home</a>
+                            <a class="nav-link text-dark" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-light" href="assignments.php">Assignments</a>
+                            <a class="nav-link text-dark" href="assignments.php">Assignments</a>
                         </li>
                     </ul>
                 </div>
