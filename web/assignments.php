9c37055a694e19855db8539be411e02b9bf1a7ba
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 15:31:13 -0700

    li padding assign

--- web/assignments.php
+++ web/assignments.php
@@ -14,12 +14,12 @@
             <div class="row">
                <ul class="list-group w-50 bg-light main-assign">
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 02</li>
-                    <li id="week-02" class="list-group-item p-0">                    
+                    <li id="week-02" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
-                            <li class="py-1"><a class="ml-2" href="Week-2/02-teach.php">02 Teach: Team Activity</a></li>
-                            <li class="py-1"><a class="ml-2" href="Week-2/assignments.php">02 Prove: Assignment page</a></li>
-                            <li class="py-1"><a class="ml-2" href="Week-2/index.php">02 Prove: Home page</a></li>
-                            <li class="pb-2 pt-1"><a class="ml-2" href="#">Coming soon</a></li>
+                            <li class="pl-2 py-1"><a href="Week-2/02-teach.php">02 Teach: Team Activity</a></li>
+                            <li class="pl-2 py-1"><a href="Week-2/assignments.php">02 Prove: Assignment page</a></li>
+                            <li class="pl-2 py-1"><a href="Week-2/index.php">02 Prove: Home page</a></li>
+                            <li class="pl-2 pb-2 pt-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                 </ul>
