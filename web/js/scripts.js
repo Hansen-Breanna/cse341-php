f6d0b11ae0f623c66c0be9bb939379b0a0366706
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 15:56:09 -0700

    change toggle

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,7 +1,7 @@
 // Toggle visibility of week assignments
 // https://api.jquery.com/slidetoggle/
 $(document).ready(function(){
-    $("#vis-wk-02").hide();
+    $("#week-02").hide();
 
     $("#vis-wk-02").click(function(){
       $("#week-02").removeClass('d-none');
