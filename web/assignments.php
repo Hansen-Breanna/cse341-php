25721df39cd8a98ae455202349aa0489b765c88e
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 16:08:23 -0700

    add scripts for all weeks

--- web/assignments.php
+++ web/assignments.php
@@ -4,7 +4,7 @@
 <!-- Header -->
 <?php include 'common/header.php'; ?>
 
-                <h1>Assignments</h1>
+                <h1 class="display-2">Assignments</h1>
             </div>
         </div>
     </header>
@@ -23,73 +23,73 @@
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 03</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-03" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 04</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-04" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 05</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-05" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 06</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-06" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 07</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-07" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 08</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-08" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 09</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-09" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 10</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-10" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 11</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-11" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 12</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-12" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 13</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-13" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
                     </li>
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 14</li>
-                    <li id="week-02" class="list-group-item">                    
+                    <li id="week-14" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
                             <li class="py-1"><a href="#">Coming soon</a></li>
                         </ul>
--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,9 +1,59 @@
 // Toggle visibility of week assignments
 // https://api.jquery.com/slidetoggle/
 $(document).ready(function(){
+    // initially hide all weeks
     $("#week-02").hide();
+    $("#week-03").hide();
+    $("#week-04").hide();
+    $("#week-05").hide();
+    $("#week-06").hide();
+    $("#week-07").hide();
+    $("#week-08").hide();
+    $("#week-09").hide();
+    $("#week-10").hide();
+    $("#week-11").hide();
+    $("#week-12").hide();
+    $("#week-13").hide();
+    $("#week-14").hide();
 
+    // Toggle weeks
     $("#vis-wk-02").click(function(){
       $("#week-02").toggle();
     });
+    $("#vis-wk-03").click(function(){
+        $("#week-03").toggle();
+      });
+      $("#vis-wk-04").click(function(){
+        $("#week-04").toggle();
+      });
+      $("#vis-wk-05").click(function(){
+        $("#week-05").toggle();
+      });
+      $("#vis-wk-06").click(function(){
+        $("#week-06").toggle();
+      });
+      $("#vis-wk-07").click(function(){
+        $("#week-07").toggle();
+      });
+      $("#vis-wk-08").click(function(){
+        $("#week-08").toggle();
+      });
+      $("#vis-wk-09").click(function(){
+        $("#week-09").toggle();
+      });
+      $("#vis-wk-10").click(function(){
+        $("#week-10").toggle();
+      });
+      $("#vis-wk-11").click(function(){
+        $("#week-11").toggle();
+      });
+      $("#vis-wk-12").click(function(){
+        $("#week-12").toggle();
+      });
+      $("#vis-wk-13").click(function(){
+        $("#week-13").toggle();
+      });
+      $("#vis-wk-14").click(function(){
+        $("#week-14").toggle();
+      });
   });
