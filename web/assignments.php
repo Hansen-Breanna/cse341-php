08996599f71a932bb547eaa45267a2bc67cbff56
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 12:10:28 -0700

    fix toggle

--- web/assignments.php
+++ web/assignments.php
@@ -14,11 +14,11 @@
         <div class="container">
             <div class="row">
                <ul class="list-group w-50 bg-light main-assign">
-                    <li id="vis-wk-2" class="list-group-item list-group-item-action">Week 02
-                        <ul class="list-group-item-action p-0 assign week-02">
-                            <li class="list-group-item-action pt-2"><a href="Week-2/02-teach.php">02 Teach: Team Activity</li>
-                            <li class="list-group-item-action pt-2"><a href="Week-2/assignments.php">02 Prove: Assignment page</li>
-                            <li class="list-group-item-action pt-2"><a href="Week-2/index.php">02 Prove: Home page</li>
+                    <li id="vis-wk-2" class="list-group-item">Week 02
+                        <ul id="week-02" class="list-group-item-action p-0 assign">
+                            <li class="pt-2"><a href="Week-2/02-teach.php">02 Teach: Team Activity</li>
+                            <li class="pt-2"><a href="Week-2/assignments.php">02 Prove: Assignment page</li>
+                            <li class="pt-2"><a href="Week-2/index.php">02 Prove: Home page</li>
                         </ul>
                     </li>
                 </ul>
