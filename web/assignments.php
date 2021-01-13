6238104ef64ec223a7bd2dec86339a9373443c85
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 12:01:30 -0700

    add ul class for inner ul

--- web/assignments.php
+++ web/assignments.php
@@ -15,10 +15,10 @@
             <div class="row">
                <ul class="list-group w-50">
                     <li class="list-group-item list-group-item-action">Week 02
-                        <ul class="list-group-item-action pl-0">
-                            <li class="list-group-item-action border-top"><a href="Week-2/02-teach.php">02 Teach: Team Activity</li>
-                            <li class="list-group-item-action border-top"><a href="Week-2/assignments.php">02 Prove: Assignment page</li>
-                            <li class="list-group-item-action border-top"><a href="Week-2/index.php">02 Prove: Home page</li>
+                        <ul class="list-group-item-action pl-0 assign">
+                            <li class="list-group-item-action border-top pt-2"><a href="Week-2/02-teach.php">02 Teach: Team Activity</li>
+                            <li class="list-group-item-action border-top pt-2"><a href="Week-2/assignments.php">02 Prove: Assignment page</li>
+                            <li class="list-group-item-action border-top pt-2"><a href="Week-2/index.php">02 Prove: Home page</li>
                         </ul>
                     </li>
                 </ul>
