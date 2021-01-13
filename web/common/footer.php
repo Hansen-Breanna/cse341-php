f5f7346f49d4483f2c5b48592dae2281c0e4a659
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 15:10:30 -0700

    change footer

--- web/common/footer.php
+++ web/common/footer.php
@@ -3,7 +3,7 @@
 <footer class="bg-secondary mt-3 text-light py-3">
     <div class="container">
         <div class="row">
-            <?php echo "&copy; " + date("Y");?> 
+            <?php echo "<p class='text-light'>&copy; " + date("Y") + "</p>";?> 
         </div>
     </div>
 </footer>
--- web/index.php
+++ web/index.php
@@ -15,21 +15,21 @@
                 <div id="box-1" class="shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-0 col-md d-flex flex-column">
                   <h2 class="text-center display-5 py-3">Outdoors</h2>
                   <img class="offset-1 col-10" src="images/outdoors.jpg" alt="Mountains behind a lake and trees">
-                  <p class="py-3">I love to enjoy the outdoors</p>
+                  <p class="py-3 offset-1 col-10">I love to enjoy the outdoors</p>
                 </div>
                 <div id="box-2" class="shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-1 col-md">
                   <h2 class="text-center display-5 py-3">Games</h2>
                   <img class="offset-1 col-10" src="images/dominion-strategy-game.jpg" alt="Dominion Strategy Games">
-                  <p class="py-3">My kids and I love to play games together.</p>
+                  <p class="py-3 offset-1 col-10">My kids and I love to play games together.</p>
                 </div>
                 <div id="box-3" class="shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-1 col-md">
                   <h2 class="text-center display-5 py-3">Reading</h2>
                   <img class="offset-1 col-10" src="images/library.jpg" alt="Large public library">
-                  <p class="py-3">I love to read.</p>
+                  <p class="py-3 offset-1 col-10">I love to read.</p>
                 </div>
             </div> 
             <div class="row col-6 offset-3 col-md-8 offset-md-2">
-                <button type="button" class="col-md btn btn-lg bg-info mt-5 text-light shadow-lg">Assignments</button>
+                <button type="button" class="col-md btn btn-lg bg-info mt-5 text-dark shadow-lg">Assignments</button>
             </div> 
         </div>
       </main>
