b3234dc0f80515435aafbdea22784bd774d5a5ce
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 12:28:03 -0700

    change toggle id to li

--- web/assignments.php
+++ web/assignments.php
@@ -15,8 +15,8 @@
             <div class="row">
                <ul class="list-group w-50 bg-light main-assign">
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 02</li>
-                    <li class="list-group-item">                    
-                        <ul id="week-02 p-0" class="list-group-item-action p-0 assign">
+                    <li id="week-02" class="list-group-item p-0">                    
+                        <ul class="list-group-item-action p-0 assign">
                             <li class="pt-2"><a href="Week-2/02-teach.php">02 Teach: Team Activity</li>
                             <li class="pt-2"><a href="Week-2/assignments.php">02 Prove: Assignment page</li>
                             <li class="pt-2"><a href="Week-2/index.php">02 Prove: Home page</li>
