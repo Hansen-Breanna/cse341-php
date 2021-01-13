b047665845f17df81b22423a43377fddd753f0c0
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 11:58:28 -0700

    change list-style-type

--- web/assignments.php
+++ web/assignments.php
@@ -15,7 +15,7 @@
             <div class="row">
                <ul class="list-group w-50">
                     <li class="list-group-item list-group-item-action">Week 02
-                        <ul class="list-group-item-action pl-0 text-decoration-none">
+                        <ul class="list-group-item-action pl-0">
                             <li class="list-group-item-action border-top"><a href="Week-2/02-teach.php">02 Teach: Team Activity</li>
                             <li class="list-group-item-action border-top"><a href="Week-2/assignments.php">02 Prove: Assignment page</li>
                             <li class="list-group-item-action border-top"><a href="Week-2/index.php">02 Prove: Home page</li>
