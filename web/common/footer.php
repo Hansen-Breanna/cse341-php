af0e76506a7e2faa7613cc12c5233d523a0cc659
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 15:14:52 -0700

    remove footer p padding

--- web/common/footer.php
+++ web/common/footer.php
@@ -3,7 +3,7 @@
 <footer class="bg-secondary mt-3 text-light py-3">
     <div class="container">
         <div class="row">
-            <p>&copy;&nbsp; <?php echo date("Y") ;?> - Breanna Hansen</p>
+            <p class="mb-0">&copy;&nbsp; <?php echo date("Y") ;?> - Breanna Hansen</p>
         </div>
     </div>
 </footer>
