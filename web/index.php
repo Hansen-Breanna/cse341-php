cdf451356f78421527f06368cd4d8d612ad0a6eb
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 14:50:53 -0700

    add image content home

--- web/index.php
+++ web/index.php
@@ -12,22 +12,28 @@
     
     <main>
       <div class="container">
-          <div class="row d-md-flex">
-              <div id="box-1" class="main-image shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-0 col-md d-flex flex-column">
-                Outdoors
-              </div>
-              <div id="box-2" class="shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-1 col-md">
-                Games
-              </div>
-              <div id="box-3" class="shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-1 col-md">
-                Reading
-              </div>
-          </div> 
-          <div class="row col-6 offset-3 col-md-8 offset-md-2">
-              <button type="button" class="col-md btn btn-lg bg-primary mt-5 text-light shadow-lg">Assignments</button>
-          </div> 
-      </div>
-    </main>
-  </div>
+            <div class="row d-md-flex">
+                <div id="box-1" class="shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-0 col-md d-flex flex-column">
+                  <h2>Outdoors</h2>
+                  <img src="images/outdoors.jpg" alt="Mountains behind a lake and trees">
+                  <p>I love to enjoy the outdoors</p>
+                </div>
+                <div id="box-2" class="shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-1 col-md">
+                  <h2>Games</h2>
+                  <img src="images/dominion-strategy-game.jpg" alt="Dominion Strategy Games">
+                  <p>My kids and I love to play games together.</p>
+                </div>
+                <div id="box-3" class="shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-1 col-md">
+                  <h2>Reading</h2>
+                  <img src="images/library.jpg" alt="Large public library">
+                  <p>I love to read.</p>
+                </div>
+            </div> 
+            <div class="row col-6 offset-3 col-md-8 offset-md-2">
+                <button type="button" class="col-md btn btn-lg bg-primary mt-5 text-light shadow-lg">Assignments</button>
+            </div> 
+        </div>
+      </main>
+    </div>
 <!-- Footer -->
 <?php include 'common/footer.php'; ?>
