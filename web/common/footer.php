6c4564cd9773f18ce8d33ab03493f0b276a175c4
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 15:12:44 -0700

    change footer content

--- web/common/footer.php
+++ web/common/footer.php
@@ -3,7 +3,7 @@
 <footer class="bg-secondary mt-3 text-light py-3">
     <div class="container">
         <div class="row">
-            <?php echo "<p class='text-light'>&copy; " + date("Y") + "</p>";?> 
+            <p>&copy;&nbsp; <?php echo date("Y") ;?> - Breanna Hansen</p>
         </div>
     </div>
 </footer>
