11d5046d860eba3d94488d4fe614fb51b8d6d78c
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 15:32:05 -0700

    li padding test assign

--- web/assignments.php
+++ web/assignments.php
@@ -16,7 +16,7 @@
                     <li class="list-group-item" id="vis-wk-02"> &#9660; Week 02</li>
                     <li id="week-02" class="list-group-item">                    
                         <ul class="list-group-item-action p-0 assign">
-                            <li class="pl-2 py-1"><a href="Week-2/02-teach.php">02 Teach: Team Activity</a></li>
+                            <li class="py-1"><a href="Week-2/02-teach.php">02 Teach: Team Activity</a></li>
                             <li class="pl-2 py-1"><a href="Week-2/assignments.php">02 Prove: Assignment page</a></li>
                             <li class="pl-2 py-1"><a href="Week-2/index.php">02 Prove: Home page</a></li>
                             <li class="pl-2 pb-2 pt-1"><a href="#">Coming soon</a></li>
