d49993ad4944aa099a11d640b4cffce50de83686
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 11:41:55 -0700

    add week 2 links to assignments

--- web/assignments.php
+++ web/assignments.php
@@ -16,7 +16,9 @@
                <ul class="list-group w-50">
                     <li class="list-group-item list-group-item-action">Week 02
                         <ul class="list-group-item-action">
-                            <li class="list-group-item-action"><a href="Week-2/02-teach.php">02 Teach: Team Activity</li>
+                            <li class="list-group-item-action border-top"><a href="Week-2/02-teach.php">02 Teach: Team Activity</li>
+                            <li class="list-group-item-action border-top"><a href="Week-2/assignments.php">02 Prove: Assignment page</li>
+                            <li class="list-group-item-action border-top"><a href="Week-2/index.php">02 Prove: Home page</li>
                         </ul>
                     </li>
                 </ul>
