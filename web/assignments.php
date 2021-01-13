004661ca63270f784fb71e184af3b4ac11a24a29
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 12:16:06 -0700

    add assignments button

--- web/assignments.php
+++ web/assignments.php
@@ -14,7 +14,7 @@
         <div class="container">
             <div class="row">
                <ul class="list-group w-50 bg-light main-assign">
-                    <li id="vis-wk-2" class="list-group-item">Week 02
+                    <li class="list-group-item"><button type="button" id="vis-wk-02" class="p-2">&#9660;</button>Week 02
                         <ul id="week-02" class="list-group-item-action p-0 assign">
                             <li class="pt-2"><a href="Week-2/02-teach.php">02 Teach: Team Activity</li>
                             <li class="pt-2"><a href="Week-2/assignments.php">02 Prove: Assignment page</li>
