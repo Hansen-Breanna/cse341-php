947487ba7523087bb32376f88bc3049ec6be6920
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 23:04:00 -0700

    test nav collapse

--- web/common/nav.php
+++ web/common/nav.php
@@ -4,21 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>02 Teach: Team Activity</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="../js/scripts.js"></script>
+    <title>02 Teach: Team Activity</title>
 </head>
 <body class="bg-dark h-100 m-0">
     <header>
         <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-dark border-bottom box-shadow mb-3">
             <div class="container">
                 <a class="navbar-brand text-dark">CSE 341 Software Engineering I</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbar-collapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
+                <div id="navbar-collapse" class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
                     <ul class="navbar-nav flex-grow-1">
                         <li class="nav-item">
                             <a class="nav-link text-dark" href="index.php">Home</a>
