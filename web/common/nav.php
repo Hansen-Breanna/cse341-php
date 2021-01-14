f488a4b959deeada9f7bd8d9c19f147abd5bcd36
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 23:09:41 -0700

    sample nav

--- web/common/nav.php
+++ web/common/nav.php
@@ -12,7 +12,39 @@
 </head>
 <body class="bg-dark h-100 m-0">
     <header>
-        <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-dark border-bottom box-shadow mb-3">
+
+    <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+	    <div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		  <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span> 
+          </button>
+		  <a class="navbar-brand" href="#">EJ Media</a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+	  <ul class="nav navbar-nav">
+	    <li class="active"><a href="#">Home</a></li>
+		<li><a href="#">News</a></li>
+		<li><a href="#">Lifestyle</a></li>
+		<li><a href="#">Entertainment</a></li>
+	  </ul> 
+	    <ul class="nav navbar-nav navbar-right">
+		<li><a href="#"><span class="glyphicon glyphicon-envelope"></span> Contact Us</a></li>
+		<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+	  </ul>            
+	  <form class="navbar-form navbar-right">
+	     <div class="form-group">
+		     <input type="text" class="form-control" placeholder="Search">
+		 </div>
+		 <button type="submit" class="btn btn-danger">Submit</button>
+          </form>
+    </div>  
+   </div>  
+  </nav>
+  
+        <nav class="navbar bg-info navbar-dark text-dark border-bottom box-shadow mb-3">
             <div class="container">
                 <a class="navbar-brand text-dark">CSE 341 Software Engineering I</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse">
