cdb660d7123a29ae3a2dc56bc0a2d932709abdea
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 23:33:08 -0700

    return to original nav

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,12 +1,12 @@
 </head>
 <body class="bg-dark h-100 m-0">
-        <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-dark border-bottom box-shadow mb-3">
+<nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-dark border-bottom box-shadow mb-3">
             <div class="container">
                 <a class="navbar-brand text-dark">CSE 341 Software Engineering I</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbar-collapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="d-sm-inline-flex flex-sm-row-reverse">
+                <div id="navbar-collapse" class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
                     <ul class="navbar-nav flex-grow-1">
                         <li class="nav-item">
                             <a class="nav-link text-dark" href="index.php">Home</a>
