57d66b02f504d08a1930a62324e722df509bc4d9
Breanna Hansen <hansbrea@hotmail.com>
2021-01-13 23:12:37 -0700

    test nav

--- web/common/nav.php
+++ web/common/nav.php
@@ -17,33 +17,22 @@
     <div class="container-fluid">
 	    <div class="navbar-header">
 		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-		  <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span> 
+            <span class="navbar-toggler-icon"></span>
           </button>
 		  <a class="navbar-brand" href="#">EJ Media</a>
-		</div>
+        </div>
+        
 		<div class="collapse navbar-collapse" id="myNavbar">
-	  <ul class="nav navbar-nav">
-	    <li class="active"><a href="#">Home</a></li>
-		<li><a href="#">News</a></li>
-		<li><a href="#">Lifestyle</a></li>
-		<li><a href="#">Entertainment</a></li>
-	  </ul> 
-	    <ul class="nav navbar-nav navbar-right">
-		<li><a href="#"><span class="glyphicon glyphicon-envelope"></span> Contact Us</a></li>
-		<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-	  </ul>            
-	  <form class="navbar-form navbar-right">
-	     <div class="form-group">
-		     <input type="text" class="form-control" placeholder="Search">
-		 </div>
-		 <button type="submit" class="btn btn-danger">Submit</button>
-          </form>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Lifestyle</a></li>
+                <li><a href="#">Entertainment</a></li>
+            </ul>           
     </div>  
    </div>  
   </nav>
-  
+
         <nav class="navbar bg-info navbar-dark text-dark border-bottom box-shadow mb-3">
             <div class="container">
                 <a class="navbar-brand text-dark">CSE 341 Software Engineering I</a>
