1e2e9b1d023a4f499a6d254885c17788881e7e54
Breanna Hansen <hansbrea@hotmail.com>
2021-01-14 21:54:55 -0700

    remove w3school styling

--- web/common/nav.php
+++ web/common/nav.php
@@ -12,14 +12,14 @@
         </a>
     </div>
 
-    <nav class="navbar navbar-expand-sm bg-success navbar-toggleable-sm navbar-dark text-light border-bottom box-shadow mb-3">
+    <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-light border-bottom box-shadow mb-3">
             <div class="container">
-                <a class="navbar-brand text-light">CSE 341 Software Engineering I</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" onclick="navToggle()">
+                <a class="navbar-brand text-light" href="index.php">CSE 341 Software Engineering I</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
-                    <ul id="topnav" class="navbar-nav flex-grow-1">
+                    <ul class="navbar-nav flex-grow-1">
                         <li class="nav-item">
                             <a class="nav-link text-light" href="index.php">Home</a>
                         </li>
