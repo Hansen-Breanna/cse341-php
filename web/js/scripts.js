da731f4f521307e1cd6241da364eca2ea2678d9f
Breanna Hansen <hansbrea@hotmail.com>
2021-01-14 20:43:08 -0700

    add id to ul in nav

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -60,7 +60,7 @@ $(document).ready(function(){
 
   // Nav toggle
   function navToggle() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("navbar-nav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
