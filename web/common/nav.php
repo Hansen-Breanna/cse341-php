133e6e8fb3b7068912f67ac3c4c243c4e1b71601
Breanna Hansen <hansbrea@hotmail.com>
2021-01-14 23:50:55 -0700

    remove nav button

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,11 +1,8 @@
 </head>
 <body class="bg-dark h-100 m-0">
-        <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-ldark border-bottom box-shadow mb-3">
+        <nav class="navbar navbar-expand-md bg-info navbar-dark text-ldark border-bottom box-shadow mb-3">
             <div class="container">
                 <a class="navbar-brand text-dark" href="index.php">CSE 341 Software Engineering I</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
                 <div class="d-sm-inline-flex flex-sm-row-reverse">
                     <ul class="navbar-nav flex-grow-1">
                         <li class="nav-item">
