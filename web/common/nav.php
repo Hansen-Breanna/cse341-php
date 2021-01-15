88f64817b267c8ed4d6b850e8204665bd2d696ad
Breanna Hansen <hansbrea@hotmail.com>
2021-01-14 20:40:59 -0700

    test topnav

--- web/common/nav.php
+++ web/common/nav.php
@@ -13,12 +13,12 @@
 </div>
 
         <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-dark border-bottom box-shadow mb-3">
-            <div class="container top-nav">
+            <div class="container">
                 <a class="navbar-brand text-dark">CSE 341 Software Engineering I</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" onclick="navToggle()">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
+                <div class="top-nav navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
                     <ul class="navbar-nav flex-grow-1">
                         <li class="nav-item">
                             <a class="nav-link text-dark" href="index.php">Home</a>
