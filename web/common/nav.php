69f9758f8961cc2b07961ee28f29e41995359366
Breanna Hansen <hansbrea@hotmail.com>
2021-01-14 21:56:49 -0700

    remove extra navs

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,45 +1,13 @@
 </head>
 <body class="bg-dark h-100 m-0">
     <header>
-    
-    <div class="topnav bg-info text-dark border-bottom box-shadow mb-3" id="nav">
-        <a href="#home" class="active">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <a href="javascript:void(0);" class="icon" onclick="navToggle()">
-        <span class="navbar-toggler-icon"></span>
-        </a>
-    </div>
-
-    <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-light border-bottom box-shadow mb-3">
+        <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-ldark border-bottom box-shadow mb-3">
             <div class="container">
-                <a class="navbar-brand text-light" href="index.php">CSE 341 Software Engineering I</a>
+                <a class="navbar-brand text-dark" href="index.php">CSE 341 Software Engineering I</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
-                    <ul class="navbar-nav flex-grow-1">
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="assignments.php">Assignments</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-dark border-bottom box-shadow mb-3">
-            <div class="container">
-                <!-- Navbar-brand and toggler icon -->
-                <a class="navbar-brand text-dark" href="index.php">CSE 341 Software Engineering I</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" onclick="navToggle()">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <!-- Link list -->
-                <div class="d-sm-inline-flex flex-sm-row-reverse">
                     <ul class="navbar-nav flex-grow-1">
                         <li class="nav-item">
                             <a class="nav-link text-dark" href="index.php">Home</a>
