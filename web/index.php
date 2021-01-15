94c07cf1f54ff5643675806dad7cea05b958e929
Breanna Hansen <hansbrea@hotmail.com>
2021-01-15 01:21:54 -0700

    add content

--- web/index.php
+++ web/index.php
@@ -20,17 +20,17 @@
                 <div onmouseover="mOver(this)" onmouseout="mOut(this)" id="box-1" class="bg-light shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-0 col-md">
                   <h2 class="text-center display-5 py-3">Outdoors</h2>
                   <img class="offset-1 col-10" src="images/outdoors.jpg" alt="Mountains behind a lake and trees">
-                  <p class="py-3 offset-1 col-10">I love to enjoy the outdoors</p>
+                  <p class="py-3 offset-1 col-10">I love to enjoy hike and play in the outdoors. </p>
                 </div>
                 <div onmouseover="mOver(this)" onmouseout="mOut(this)" id="box-2" class="bg-light shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-1 col-md">
                   <h2 class="text-center display-5 py-3">Games</h2>
                   <img class="offset-1 col-10" src="images/dominion-strategy-game.jpg" alt="Dominion Strategy Games">
-                  <p class="py-3 offset-1 col-10">My kids and I love to play games together.</p>
+                  <p class="py-3 offset-1 col-10">My kids and I love to play strategy games together.</p>
                 </div>
                 <div onmouseover="mOver(this)" onmouseout="mOut(this)" id="box-3" class="bg-light shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-1 col-md">
                   <h2 class="text-center display-5 py-3">Reading</h2>
                   <img class="offset-1 col-10" src="images/library.jpg" alt="Large public library">
-                  <p class="py-3 offset-1 col-10">I love to read.</p>
+                  <p class="py-3 offset-1 col-10">I love to read. My favorite books are historical fiction.</p>
                 </div>
             </div> 
             <div class="row col-6 offset-3 col-md-8 offset-md-2">
