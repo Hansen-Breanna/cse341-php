270c592bc522f9711abb2ab812cedf912b3fafbf
Breanna Hansen <hansbrea@hotmail.com>
2021-01-14 23:04:15 -0700

    remove files

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,6 +1,5 @@
 </head>
 <body class="bg-dark h-100 m-0">
-    <header>
         <nav class="navbar navbar-expand-sm bg-info navbar-toggleable-sm navbar-dark text-ldark border-bottom box-shadow mb-3">
             <div class="container">
                 <a class="navbar-brand text-dark" href="index.php">CSE 341 Software Engineering I</a>
@@ -19,28 +18,5 @@
                 </div>
             </div>
         </nav>
-    </header>
 
-    <header>
-        <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-dark border-bottom box-shadow mb-3">
-            <div class="container">
-                <a class="navbar-brand text-light" asp-page="/Scriptures/Index">My Scripture Journal</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
-                    <ul class="navbar-nav flex-grow-1">
-                        <li class="nav-item">
-                            <a class="nav-link text-light" asp-area="" asp-page="/Index">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" asp-area="" asp-page="/Scriptures/About">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" asp-area="" asp-page="/Contact">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    
