64fc3d7fbe2b5009b37b951ed705782e45772024
Breanna Hansen <hansbrea@hotmail.com>
2021-01-16 16:13:20 -0700

    test recipes

---
+++ web/recipes.js
@@ -0,0 +1,15 @@
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://webknox-recipes.p.rapidapi.com/recipes/cuisine",
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"x-rapidapi-key": "6288b04b82msh924ce912f68ff81p1c951cjsn7b55d5b23d03",
+		"x-rapidapi-host": "webknox-recipes.p.rapidapi.com"
+	},
+	"data": {
+		"title": "Pork roast with green beans",
+		"ingredientList": "3 oz pork shoulder"
+	}
+};
