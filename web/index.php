a8c6753f65e50ffcb6b61541955c0d048374ce88
Breanna Hansen <hansbrea@hotmail.com>
2021-01-16 12:48:13 -0700

    add box-1 back

--- web/index.php
+++ web/index.php
@@ -17,11 +17,23 @@
     <main class="mb-5">
       <div class="container">
             <div class="row d-md-flex">
+            <div>
                 <div onmouseover="mOver(this)" onmouseout="mOut(this)" id="box-1" class="bg-light shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-0 col-md">
                   <h2 class="text-center display-5 py-3">Outdoors</h2>
                   <img class="offset-1 col-10" src="images/outdoors.jpg" alt="Mountains behind a lake and trees">
                   <p class="py-3 offset-1 col-10">I love to enjoy hike and play in the outdoors. </p>
                 </div>
+                <div id="box-1-back"> 
+                    <ul>
+                      <li>Warm River</li>
+                      <li>Yellowstone National Park</li>
+                      <li>Redfish Lake</li>
+                      <li>Bear Lake</li>
+                      <li>Jackson Hole</li>
+                      <li>Grand Teton National Park</li>
+                    </ul>
+                </div>
+            </div>
                 <div onmouseover="mOver(this)" onmouseout="mOut(this)" id="box-2" class="bg-light shadow-lg rounded box p-2 mt-4 offset-2 col-8 offset-md-1 col-md">
                   <h2 class="text-center display-5 py-3">Games</h2>
                   <img class="offset-1 col-10" src="images/dominion-strategy-game.jpg" alt="Dominion Strategy Games">
