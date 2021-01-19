076a6ca07eeda8f604f6cb34f938e18fca39ba9e
Breanna Hansen <hansbrea@hotmail.com>
2021-01-18 23:38:14 -0700

    first form to week 3

--- web/Week-3/03-teach.php
+++ web/Week-3/03-teach.php
@@ -18,17 +18,29 @@
       <div class="container">
             <div class="row d-md-flex">
 
-            <form action="welcome.php" method="post">
-                Name: <input type="text" name="name"><br>
-                E-mail: <input type="text" name="email"><br>
-                Major:
-                <input type="radio" name="major" value="Computer Science">Computer Science
-                <input type="radio" name="major" value="Web Design & Development">Web Design & Development
-                <input type="radio" name="major" value="Computer Information Technology">Computer Information Technology
-                <input type="radio" name="major" value="Computer Engineering">Computer Engineering
-                Comments:
-                <textarea name="comment" rows=5 cols="40"></textarea>
-                <input type="submit">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <div class="name">
+                    <label for="name">Name:</label> 
+                    <input type="text" name="name"><br>
+                </div>
+                <div class="email">
+                    <label for="email">Email:</label>
+                    <input type="text" name="email"><br>
+                </div>
+                <div class="major">
+                    <label for="major">Major:</label>
+                    <input type="radio" name="major" value="Computer Science">Computer Science
+                    <input type="radio" name="major" value="Web Design & Development">Web Design & Development
+                    <input type="radio" name="major" value="Computer Information Technology">Computer Information Technology
+                    <input type="radio" name="major" value="Computer Engineering">Computer Engineering
+                </div>
+                <div class="comments">
+                    <label for="comments">Comments:</label>
+                    <textarea name="comment" rows=5 cols="40"></textarea>
+                </div>
+                <div class="submit">
+                    <input type="submit">
+                </div>
             </form>
 
             </div>
