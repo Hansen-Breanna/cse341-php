e17eeb2d7e4a84012c7cf3df2b7680df20338ddf
Breanna Hansen <hansbrea@hotmail.com>
2021-01-18 23:47:50 -0700

    week 3 output

---
+++ web/Week-3/03-teach-display.php
@@ -0,0 +1,27 @@
+<!-- Head -->
+<?php include '$_SERVER["common/head.php"]'; ?>
+
+    <title>02 Teach: Team Activity</title>
+
+<!-- Nav -->
+<?php include '$_SERVER["common/nav.php"]'; ?>
+
+<!-- Header -->
+<?php include '$_SERVER["common/header.php"]'; ?>
+
+              <h1 class="offset-1 col-10 offset-md-0 col-md-12">Breanna Hansen</h1>
+          </div>
+      </div>
+    </header>
+    
+    <main class="mb-5">
+      <div class="container">
+        <div class="row d-md-flex">
+            Welcome <?php echo $_POST["name"]; ?><br>
+            Your email address is: <?php echo $_POST["email"]; ?>
+        </div>
+      </div>
+    </main>
+
+<!-- Footer -->
+<?php include '$_SERVER["common/footer.php"]'; ?>
