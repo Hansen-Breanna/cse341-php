a4e2720d8102323b2679a5e9f18d0f7b7f2863a0
Breanna Hansen <hansbrea@hotmail.com>
2021-01-20 16:33:50 -0700

    put continents in div

--- web/Week-3/team/week3/index.php
+++ web/Week-3/team/week3/index.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="../../../css/style.css" />
 </head>
 
-<body>
+<body class="bg-secondary text-light">
   <form action="form.php" method="POST">    
     <div class="name">
         <label for="name">Name:</label> 
@@ -18,26 +18,25 @@
         <label for="email">Email:</label>
         <input type="text" name="email" id="email"><br>
     </div>
-
-    <div class="major">>
+    <div class="major">
         <label for="major">Major:</label><br>
-
         <?php
-
           $majors = array("CS"=>"Computer Science", "WDD"=>"Web Design and Development", "CIT"=>"Computer Information Technology", "CE"=>"Computer Engineering");
-
+          
           //Create major radio buttons
           foreach ($majors as $key => $value) {
             echo '<input type="radio" id="'.$key.'" name="major" value="'.$value.'">
             <label for="'.$key.'">'.$value.'</label><br>';
           }
         ?>
-
 </div>
-    <label for="comments">Comments</label>
-    <textarea id="comments" name="comments"></textarea><br><br>
+<div class="comments">
+    <label for="comment">Comments:</label><br>
+    <textarea name="comment" rows=5 cols="40"></textarea>
+</div>
 
-    <p><strong><u>Which continents have you visited?</u></strong></p>
+<div class="continents">
+    <label for="continents[]">Which continents have you visited?</label>
     <input type="checkbox" id="nAmerica" name="continents[]" value="na">
     <label for="nAmerica">North America</label><br>
     <input type="checkbox" id="sAmerica" name="continents[]" value="sa">
@@ -52,6 +51,7 @@
     <label for="africa">Africa</label><br>
     <input type="checkbox" id="antarctica" name="continents[]" value="an">
     <label for="antarctica">Antarctica</label><br><br><br>
+</div>    
 
     <input type="submit">
   </form>
