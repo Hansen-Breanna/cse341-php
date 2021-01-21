055a508cb66c036524027a3bf892b122ec1c9419
Breanna Hansen <hansbrea@hotmail.com>
2021-01-20 18:51:44 -0700

    text

--- web/Week-3/team/week3/form.php
+++ web/Week-3/team/week3/form.php
@@ -32,7 +32,7 @@ function test_input($data) {
 }
 ?>
 
-<?php echo $continents[0]; ?>
+<?php echo var_dump($_POST);  var_dump($continents[0]);?>
   <div>
     <label>Name: </label> <?php echo $_POST["name"]; ?>
   </div>
