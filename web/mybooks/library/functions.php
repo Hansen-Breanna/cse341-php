a41819164a33ee3d93b4fae6f19055e977ad6067
Breanna Hansen <hansbrea@hotmail.com>
2021-01-28 22:07:54 -0700

    add content to catalog

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -27,7 +27,7 @@ function getAuthorName($author_id) {
 }
 
 function displayCatalog($catalog) {
-   $bookList = '<tbody class="table table-striped table-hover">';
+   $bookList = '<tbody class="table table-striped table-hover text-light">';
    foreach ($catalog as $book) {
       $bookList .= '<tr><td>' . $book['title_of_book'] . '</td><td class="pl-5">' . getAuthorName($book['author_id']) . '</td></tr>';
    }
