b40d420f4d8c0fb400e5116db1f6cf73f6e03636
Breanna Hansen <hansbrea@hotmail.com>
2021-01-28 23:01:54 -0700

    add content to wish list

---
+++ web/mybooks/js/scripts.js
@@ -0,0 +1,8 @@
+// Toggle visibility of week assignments
+// https://api.jquery.com/slidetoggle/
+$(document).ready(function(){
+      // Nav toggle
+      $(".navbar-toggler").click(function(){
+        $("#link-nav").toggle();
+      });
+  });
