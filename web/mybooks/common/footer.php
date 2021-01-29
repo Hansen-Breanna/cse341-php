b8efeed4c6855e05279f0f4f6ac4549e35c96ffb
Breanna Hansen <hansbrea@hotmail.com>
2021-01-28 22:57:33 -0700

    add content to wish list

--- web/mybooks/common/footer.php
+++ web/mybooks/common/footer.php
@@ -1,6 +1,6 @@
 </div>
 
-<footer class="bg-dark mt-3 p-3 border-top">
+<footer class="bg-info mt-3 p-3 border-top">
     <div class="container">
         <div class="row">
             <p class="mb-0">&copy;&nbsp; <?php echo date("Y") ;?> - Breanna Hansen</p>
