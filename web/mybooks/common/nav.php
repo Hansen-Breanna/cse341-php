4baf62f52c8e7c7c1736b9da782cac4f4310a219
Breanna Hansen <hansbrea@hotmail.com>
2021-01-30 23:33:39 -0700

    add catalog forms

--- web/mybooks/common/nav.php
+++ web/mybooks/common/nav.php
@@ -1,6 +1,6 @@
 </head>
 <body class="bg-dark h-100 m-0 text-light">
-        <nav class="navbar navbar-expand-md bg-info navbar-dark border-bottom box-shadow mb-3">
+        <nav class="navbar navbar-expand-lg bg-info navbar-dark border-bottom box-shadow mb-3">
             <div class="container">
                 <a class="navbar-brand" href="index.php">My Books</a>
                 <button class="navbar-toggler" type="button">
@@ -15,16 +15,16 @@
                             <a class="nav-link" href="index.php?action=catalog">Catalog</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link d-md-none d-lg-inline" href="index.php?action=wish">Wish List</a>
+                            <a class="nav-link" href="index.php?action=wish">Wish List</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link d-md-none d-lg-inline" href="index.php?action=favorite">Favorite Authors</a>
+                            <a class="nav-link" href="index.php?action=favorite">Favorite Authors</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link d-md-none d-lg-inline" href='index.php?action=blacklist'>Blacklisted Authors</a>
+                            <a class="nav-link" href='index.php?action=blacklist'>Blacklisted Authors</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link d-md-none d-lg-inline" href="index.php?action=loans">Loans</a>
+                            <a class="nav-link" href="index.php?action=loans">Loans</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="index.php?action=reviews">Reviews</a>
