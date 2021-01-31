73f9a3dedae15d5448bf2d645c09fc48d7c415a4
Breanna Hansen <hansbrea@hotmail.com>
2021-01-30 23:32:29 -0700

    add catalog forms

--- web/mybooks/common/nav.php
+++ web/mybooks/common/nav.php
@@ -15,16 +15,16 @@
                             <a class="nav-link" href="index.php?action=catalog">Catalog</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php?action=wish">Wish List</a>
+                            <a class="nav-link d-md-none d-lg-inline" href="index.php?action=wish">Wish List</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php?action=favorite">Favorite Authors</a>
+                            <a class="nav-link d-md-none d-lg-inline" href="index.php?action=favorite">Favorite Authors</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href='index.php?action=blacklist'>Blacklisted Authors</a>
+                            <a class="nav-link d-md-none d-lg-inline" href='index.php?action=blacklist'>Blacklisted Authors</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php?action=loans">Loans</a>
+                            <a class="nav-link d-md-none d-lg-inline" href="index.php?action=loans">Loans</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="index.php?action=reviews">Reviews</a>
