ced2bc6759e054776c4c93cbfc72d2f4bac60283
Breanna Hansen <hansbrea@hotmail.com>
2021-01-30 23:34:10 -0700

    add catalog forms

--- web/mybooks/view/catalog.php
+++ web/mybooks/view/catalog.php
@@ -38,7 +38,7 @@
               </div>
             </form>
           </div>
-          <div class="my-2 py-3 border-light border-top border-bottom">
+          <div class="my-3 py-2 border-secondary border-top border-bottom">
             <a href="index.php?action=add-book" class="btn btn-custom bg-orange text-dark my-2">Add Title</a>
             <a href="index.php?action=remove-book" class="btn btn-custom bg-orange text-dark my-2">Remove Title</a>
           </div>
