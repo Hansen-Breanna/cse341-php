503cde8ef30d51dd1b60a24610b65e6f7a1c0759
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 13:49:22 -0700

    content for reviews  page

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -169,7 +169,7 @@ function displayReviews($reviews) {
          }
       }   
       $reviewList .= '(' . $review['rating'] . ')</p></div>';
-      $reviewList .= '<h3>' . $details[0]['book_title'] . '</h3>';
+      $reviewList .= '<h3>' . $details[0]['title_of_book'] . '</h3>';
       $reviewList .= '<p>' . $details[0]['first_name'] . ' ' . $details[0]['middle_name'] . ' ' . $details[0]['last_name'] . '<p>';
       $reviewList .= '<p>' . $review['review'] . '<p>';
       $reviewList .= '</div>';
