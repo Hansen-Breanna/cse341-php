1b3b186fc3fbd4cc5eac8645ddd345062a73ae36
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 14:16:51 -0700

    content for reviews  page

--- web/mybooks/view/catalog.php
+++ web/mybooks/view/catalog.php
@@ -20,30 +20,6 @@
                <!-- Search Boxes -->
                <?php include 'common/search.php'; ?>
           <div>
-            <form method="post" action="index.php?action=catalog"> 
-              <div>
-                  <label>Search by Author:</label><br>
-                  <input class="rounded p-1" type="text" name="first-name" id="first-name" placeholder="First Name">
-                  <input class="rounded p-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
-                  <input type='hidden' id='session' name='session' value=''>
-              </div>
-            </form>
-            <form method="post" action="index.php?action=catalog"> 
-              <div class="mt-1">
-                  <label>Search by Title:</label><br>
-                  <div class="d-flex align-items-end">
-                    <input class="rounded p-1" type="text" name="title" id="title" placeholder="Title">
-                    <input type='hidden' id='session' name='session' value=''>
-                    <input type="submit" class="btn btn-custom bg-info ml-1">
-                  </div>
-              </div>
-            </form>
-          </div>
-          <div class="my-3 py-2 border-secondary border-top border-bottom">
-            <a href="index.php?action=add-book" class="btn btn-custom bg-orange text-dark my-2">Add Title</a>
-            <a href="index.php?action=remove-book" class="btn btn-custom bg-orange text-dark my-2">Remove Title</a>
-          </div>
-          <div>
             <table class="table table-dark table-striped text-light">
                 <thead>
                   <tr>
