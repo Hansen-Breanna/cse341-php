5398bfe2d6ddf5c268b5d681199a41ca956619fc
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 11:07:58 -0700

    content for authors page

--- web/mybooks/view/authors.php
+++ web/mybooks/view/authors.php
@@ -1,7 +1,7 @@
 <!-- Head -->
 <?php include 'common/head.php'; ?>
 
-    <title>My Books - Personal Library Application</title>
+<title>My Books - Personal Library Application</title>
 
 <!-- Nav -->
 <?php include 'common/nav.php'; ?>
@@ -9,54 +9,56 @@
 <!-- Header -->
 <?php include '../common/header.php'; ?>
 
-              <h1 class="offset-1 col-10 offset-md-0 col-md-12">Authors</h1>
-          </div>
-      </div>
-    </header>
-    
-    <main class="mb-5">
-      <div class="container">
-        <div class="row d-md-flex justify-content-center">
-          <!-- Search Boxes -->
-          <?php include 'common/search.php'; ?>
+<h1 class="offset-1 col-10 offset-md-0 col-md-12">Authors</h1>
+</div>
+</div>
+</header>
 
-          <div>
-            <table class="table table-dark table-striped text-light">
-              <thead>
-                <tr>
-                  <th class="pl-5">All Authors</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php echo $authorsTable; ?>
-              </tbody>
-            </table>
-          </div>
-          <div>
+<main class="mb-5">
+  <div class="container">
+    <div class="row d-md-flex justify-content-center">
+      <!-- Search Boxes -->
+      <?php include 'common/search.php'; ?>
+
+      <div>
+        <div>
           <table class="table table-dark table-striped text-light">
-              <thead>
-                <tr>
-                  <th class="pl-5">Favorite Authors</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php echo $favorites; ?>
-              </tbody>
-            </table>
-            <table class="table table-dark table-striped text-light">
-              <thead>
-                <tr>
-                  <th class="pl-5">Blacklisted Authors</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php echo $blacklist; ?>
-              </tbody>
-            </table>
-          </div>
+            <thead>
+              <tr>
+                <th class="pl-5">All Authors</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php echo $authorsTable; ?>
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <table class="table table-dark table-striped text-light">
+            <thead>
+              <tr>
+                <th class="pl-5">Favorite Authors</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php echo $favorites; ?>
+            </tbody>
+          </table>
+          <table class="table table-dark table-striped text-light">
+            <thead>
+              <tr>
+                <th class="pl-5">Blacklisted Authors</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php echo $blacklist; ?>
+            </tbody>
+          </table>
         </div>
       </div>
-    </main>
+    </div>
+  </div>
+</main>
 
 <!-- Footer -->
 <?php include 'common/footer.php'; ?>
