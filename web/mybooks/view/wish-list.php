f5915991f0c8e2c8d4d9b00d31c8fa5d4c7fd55a
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 14:31:40 -0700

    content for reviews  page

--- web/mybooks/view/wish-list.php
+++ web/mybooks/view/wish-list.php
@@ -17,34 +17,12 @@
 <main class="mb-5">
   <div class="container">
     <div class="row d-md-flex flex-column align-items-around">
-      <div class="d-md-flex flex-column align-items-center">
-        <form method="post" action="index.php?action=catalog">
-          <div>
-            <label>Search by Author:</label><br>
-            <input class="rounded p-1" type="text" name="first-name" id="first-name" placeholder="First Name">
-            <input class="rounded p-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
-            <input type='hidden' id='session' name='session' value=''>
-          </div>
-        </form>
-        <form method="post" action="index.php?action=catalog">
-          <div class="mt-1">
-            <label>Search by Title:</label><br>
-            <div class="d-flex align-items-end">
-              <input class="rounded p-1" type="text" name="title" id="title" placeholder="Title">
-              <input type='hidden' id='session' name='session' value=''>
-              <input type="submit" class="btn btn-custom bg-info ml-1">
-            </div>
-          </div>
-        </form>
-      </div>
+      <!-- Search Boxes -->
+      <?php include 'common/search.php'; ?>
       <div class="d-md-flex justify-content-around">
         <div>
           <h2 class="pt-4">Own Wish List</h2>
-          <div class="pb-2">
-            <a href="index.php?action=add-book" class="btn btn-custom bg-orange my-2">Add Title</a>
-            <a href="index.php?action=remove-book" class="btn btn-custom bg-orange my-2">Remove Title</a>
-          </div>
-          <table class="table table-dark table-striped text-light">
+          <table class="table table-dark table-striped text-light m-2">
             <thead>
               <tr>
                 <th class="pl-1">Book Title</th>
@@ -52,17 +30,13 @@
               </tr>
             </thead>
             <?php
-            echo '<div' . $ownTable . '</div>';
+            echo '<div>' . $ownTable . '</div>';
             ?>
           </table>
         </div>
         <div>
           <h2 class="pt-4">Read Wish List</h2>
-          <div class="pb-2">
-            <a href="index.php?action=add-book" class="btn btn-custom bg-orange text-dark my-2">Add Title</a>
-            <a href="index.php?action=remove-book" class="btn btn-custom bg-orange text-dark my-2">Remove Title</a>
-          </div>
-          <table class="table table-dark table-striped text-light">
+          <table class="table table-dark table-striped text-light m-2">
             <thead>
               <tr>
                 <th class="pl-1">Book Title</th>
