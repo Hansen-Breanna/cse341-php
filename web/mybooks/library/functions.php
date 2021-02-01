fbb32b3b026e41b83e887b6ae9d25a2bc19bb5df
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 16:28:15 -0700

    get loan content

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -76,7 +76,7 @@ function getBorrower($borrower_id) {
       echo "An error occurred.\n";
       exit;
    } else {
-      $stmt = $db->query('SELECT * FROM borrower WHERE borrower_id = :id');
+      $stmt = $db->query('SELECT * FROM borrower WHERE id = :id');
       $stmt->execute(array(':id' => $borrower_id));
       $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
       var_dump($results);
@@ -148,7 +148,7 @@ function displayLoans($loans) {
    $loanList = '<tbody>';
    foreach ($loans as $loan) {
       $title = getDetails($loan['book_title_id']);
-      //$borrower = getBorrower($loan['borrower_id']);
+      $borrower = getBorrower($loan['borrower_id']);
       $loanList .= '<tr><td>' . $title[0]['title_of_book'] . '</td>';
       $loanList .= '<td>' . $borrower[0]['first_name'] . ' ' . $borrower[0]['last_name'] . '</td>';
       $loanList .= '<td>' . $loan['date_borrowed'] . '</td>';
