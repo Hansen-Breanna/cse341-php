9beab59413578420c82bafb861e6f6bb05739f4b
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 11:25:53 -0700

    content for authors page

--- web/mybooks/common/search.php
+++ web/mybooks/common/search.php
@@ -1,21 +1,21 @@
 <div>
     <form method="post" action="index.php?action=catalog">
         <div class="p-2">
-            <label>Search by Author:</label><br>
+            <label class="ml-1">Search by Author:</label><br>
             <div class="d-flex flex-wrap">
-                <input class="rounded p-1 ml-1" type="text" name="first-name" id="first-name" placeholder="First Name">
-                <input class="rounded p-1 ml-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
+                <input class="rounded p-1 m-1" type="text" name="first-name" id="first-name" placeholder="First Name">
+                <input class="rounded p-1 m-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
                 <input type='hidden' id='session' name='session' value=''>
             </div>
         </div>
     </form>
     <form method="post" action="index.php?action=catalog">
         <div class="p-2">
-            <label>Search by Title:</label><br>
+            <label class="ml-1">Search by Title:</label><br>
             <div class="d-flex flex-wrap">
-                <input class="rounded p-1 ml-1" type="text" name="title" id="title" placeholder="Title">
+                <input class="rounded p-1 m-1" type="text" name="title" id="title" placeholder="Title">
                 <input type='hidden' id='session' name='session' value=''>
-                <input type="submit" class="btn btn-custom bg-info ml-1">
+                <input type="submit" class="btn btn-custom bg-info m-1">
             </div>
         </div>
     </form>
