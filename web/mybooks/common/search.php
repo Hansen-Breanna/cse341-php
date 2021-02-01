6a2f130f38dd5e027f48f7b03df65166b4188371
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 11:19:47 -0700

    content for authors page

--- web/mybooks/common/search.php
+++ web/mybooks/common/search.php
@@ -1,19 +1,21 @@
 <div>
-    <form method="post" action="index.php?action=catalog"> 
-        <div class="d-flex flex-wrap">
+    <form method="post" action="index.php?action=catalog">
+        <div>
             <label>Search by Author:</label><br>
-            <input class="rounded p-1" type="text" name="first-name" id="first-name" placeholder="First Name">
-            <input class="rounded p-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
-            <input type='hidden' id='session' name='session' value=''>
+            <div class="d-flex flex-wrap">
+                <input class="rounded p-1" type="text" name="first-name" id="first-name" placeholder="First Name">
+                <input class="rounded p-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
+                <input type='hidden' id='session' name='session' value=''>
+            </div>
         </div>
     </form>
-    <form method="post" action="index.php?action=catalog"> 
+    <form method="post" action="index.php?action=catalog">
         <div class="mt-1 d-flex">
             <label>Search by Title:</label><br>
             <div class="d-flex align-items-end">
-            <input class="rounded p-1" type="text" name="title" id="title" placeholder="Title">
-            <input type='hidden' id='session' name='session' value=''>
-            <input type="submit" class="btn btn-custom bg-info ml-1">
+                <input class="rounded p-1" type="text" name="title" id="title" placeholder="Title">
+                <input type='hidden' id='session' name='session' value=''>
+                <input type="submit" class="btn btn-custom bg-info ml-1">
             </div>
         </div>
     </form>
