822bd33f8bc506c91d794020bc2ecff72ab6b4d7
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 11:24:40 -0700

    content for authors page

--- web/mybooks/common/search.php
+++ web/mybooks/common/search.php
@@ -3,8 +3,8 @@
         <div class="p-2">
             <label>Search by Author:</label><br>
             <div class="d-flex flex-wrap">
-                <input class="rounded p-1 pr-2" type="text" name="first-name" id="first-name" placeholder="First Name">
-                <input class="rounded p-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
+                <input class="rounded p-1 ml-1" type="text" name="first-name" id="first-name" placeholder="First Name">
+                <input class="rounded p-1 ml-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
                 <input type='hidden' id='session' name='session' value=''>
             </div>
         </div>
@@ -13,7 +13,7 @@
         <div class="p-2">
             <label>Search by Title:</label><br>
             <div class="d-flex flex-wrap">
-                <input class="rounded p-1" type="text" name="title" id="title" placeholder="Title">
+                <input class="rounded p-1 ml-1" type="text" name="title" id="title" placeholder="Title">
                 <input type='hidden' id='session' name='session' value=''>
                 <input type="submit" class="btn btn-custom bg-info ml-1">
             </div>
