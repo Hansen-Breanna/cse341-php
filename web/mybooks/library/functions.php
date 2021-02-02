1523c1fca59679519129eb88c3d6823979af0f2d
Breanna Hansen <hansbrea@hotmail.com>
2021-02-02 14:20:21 -0700

    loan list

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -140,7 +140,7 @@ function displayLoans($loans) {
       $loanList .= '<td>' . $loan['date_borrowed'] . '</td>';
       $loanList .= '<td>' . $loan['return_date'] . '</td>'; 
       $loanList .= '<td><a class="btn btn-small bg-orange" href="sms://+1' . $loan['phone_number'];
-      $loanList .= '&body=Hi%20' . $loan['first_name'] . '!%20The%20book%20you%20borrowed%20' . $loan['title_of_book'];
+      $loanList .= '?body=Hi%20' . $loan['first_name'] . '!%20The%20book%20you%20borrowed%20' . $loan['title_of_book'];
       $loanList .= '%20is%20overdue.%20Please%20contact%20me%20at%20' . $loan['user_phone'];
       $loanList .= '%20to%20return%20it.%20Thanks!">SMS</a></td>';
       $loanList .= '<a href="sms:+12087162686&amp;body= I%27d%20like%20to%20set%20up%20an%20appointmen">SMS</a>';
