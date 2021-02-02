38f616d04b3a932841fcafc46c9a3ade7ac5355b
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 22:41:35 -0700

    home page

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -11,8 +11,9 @@
         echo "An error occurred.\n";
         exit;
      } else {
-        $stmt = $db->query('SELECT b.title_of_book, a.first_name, a.middle_name, a.last_name FROM user_book u INNER JOIN book_title b ON u.book_title_id = b.id INNER JOIN author a ON a.id = b.author_id WHERE u.library_user_id = 1 ORDER BY b.title_of_book');
-        //$stmt->execute(array(':id' => $id));
+        $stmt = $db->query('SELECT b.title_of_book, a.first_name, a.middle_name, a.last_name FROM user_book u INNER JOIN book_title b ON u.book_title_id = b.id INNER JOIN author a ON a.id = b.author_id WHERE u.library_user_id = :id ORDER BY b.title_of_book');
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $results;
      }
