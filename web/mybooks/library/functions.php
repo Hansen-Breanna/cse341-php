b298a783363c4d5bb9927f7420e70a7026995f46
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 23:39:57 -0700

    wish list

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -42,7 +42,7 @@ function getUserID($username, $password) {
       echo "An error occurred.\n";
       exit;
    } else {
-      $stmt = $db->prepare('SELECT b.title_of_book, a.first_name, a.middle_name, a.last_name FROM user_book u INNER JOIN book_title b ON u.book_title_id = b.id INNER JOIN author a ON a.id = b.author_id WHERE u.library_user_id = :id AND own_wish_list = TRUE ORDER BY b.title_of_book');// WHERE own_wish_list = TRUE');
+      $stmt = $db->prepare('SELECT b.title_of_book, a.first_name, a.middle_name, a.last_name FROM user_book u INNER JOIN book_title b ON u.book_title_id = b.id INNER JOIN author a ON a.id = b.author_id WHERE u.library_user_id = :id AND own_wish_list = TRUE ORDER BY b.title_of_book');
       $stmt->bindValue(':id', $id, PDO::PARAM_INT);
       $stmt->execute();
       $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -57,10 +57,9 @@ function getReadWishes($id) {
       echo "An error occurred.\n";
       exit;
    } else {
-      $stmt = $db->query('SELECT * FROM user_book WHERE read_wish_list = TRUE');
-      //$stmt->execute(array(':id' => $id));
-      //$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-      //$stmt->execute();
+      $stmt = $db->prepare('SELECT b.title_of_book, a.first_name, a.middle_name, a.last_name FROM user_book u INNER JOIN book_title b ON u.book_title_id = b.id INNER JOIN author a ON a.id = b.author_id WHERE u.library_user_id = :id AND read_wish_list = TRUE ORDER BY b.title_of_book');
+      $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+      $stmt->execute();
       $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
       return $results;
    }
