9305df493905ce9bba9a14fad5a38f8f70de65b9
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 18:06:59 -0700

    home page

--- web/mybooks/common/nav.php
+++ web/mybooks/common/nav.php
@@ -6,8 +6,8 @@
                 <form method="post" action="index.php">
                 <label class="ml-1">Search by Title:</label><br>
             <div class="d-flex flex-wrap mb-2">
-                <input class="rounded p-1 m-1" type="text" name="title" id="title" placeholder="Title">
-                <input type='hidden' id='session' name='session' value=''>
+                <input class="rounded p-1 m-1" type="text" name="username" placeholder="username">
+                <input class="rounded p-1 m-1" type='text' name="password" placeholder="password">
                 <input type="submit" class="btn btn-custom bg-info m-1">
             </div>
 
