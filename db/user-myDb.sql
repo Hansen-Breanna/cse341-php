1cea30d6dd1d282c5b9ad9997e8dc17ce6d716fc
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 17:48:10 -0700

    sort catalog

--- db/user-myDb.sql
+++ db/user-myDb.sql
@@ -9,10 +9,11 @@ CREATE TABLE author
 );
 /* author inserts */
 INSERT INTO author (first_name, last_name) VALUES ('Jane', 'Austen');
-INSERT INTO author (first_name, last_name, is_favorite) VALUES ('Tracie', 'Peterson', TRUE);
-INSERT INTO author (first_name, last_name, is_favorite) VALUES ('Dee', 'Henderson', TRUE);
-INSERT INTO author (first_name, middle_name, last_name, is_favorite) VALUES ('Kristi', 'Ann', 'Hunter', TRUE);
-INSERT INTO author (first_name, last_name, is_favorite) VALUES ('John', 'Flanagan', TRUE);
+INSERT INTO author (first_name, last_name) VALUES ('Tracie', 'Peterson');
+INSERT INTO author (first_name, last_name) VALUES ('Dee', 'Henderson');
+INSERT INTO author (first_name, middle_name, last_name) VALUES ('Kristi', 'Ann', 'Hunter');
+INSERT INTO author (first_name, last_name) VALUES ('John', 'Flanagan');
+INSERT INTO author (first_name, last_name) VALUES ('J.K.', 'Rowling');
 /* search for favorites */
 SELECT first_name, last_name FROM author WHERE is_favorite = TRUE;
 /* create user */
@@ -21,11 +22,14 @@ CREATE TABLE library_user
 	id serial,
 	first_name VARCHAR(100) NOT NULL,
 	last_name VARCHAR(100) NOT NULL,
-	username VARCHAR(40) NOT NULL,
-	password VARCHAR(40) NOT NULL,
-	email VARCHAR(50) NOT NULL,
+	user_name VARCHAR(40) NOT NULL,
+	user_password VARCHAR(40) NOT NULL,
+	user_email VARCHAR(50) NOT NULL,
 	PRIMARY KEY (id)
 );
+INSERT INTO library_user (first_name, last_name, user_name, user_password, user_email) VALUES ('Breanna', 'Hansen', 'hansen', 'test', 'test@email.com');
+INSERT INTO library_user (first_name, last_name, user_name, user_password, user_email) VALUES ('Chase', 'Wilcox', 'wilcox', 'test', 'test@email.com');
+INSERT INTO library_user (first_name, last_name, user_name, user_password, user_email) VALUES ('Brother', 'Lyon', 'lyon', 'test', 'test@email.com');
 /* create user/author bridge table */
 CREATE TABLE user_author
 (
@@ -35,9 +39,17 @@ CREATE TABLE user_author
 	is_blacklist BOOLEAN,
 	is_favorite BOOLEAN,
 	PRIMARY KEY (id),
-	FOREIGN KEY (library_user_id) REFERENCES library_user(id)
+	FOREIGN KEY (library_user_id) REFERENCES library_user(id),
 	FOREIGN KEY (author_id) REFERENCES author(id)
 );
+INSERT INTO user_author (library_user_id, author_id, is_blacklist, is_favorite) VALUES (1, 1, FALSE, TRUE);
+INSERT INTO user_author (library_user_id, author_id, is_blacklist, is_favorite) VALUES (1, 2, FALSE, TRUE);
+INSERT INTO user_author (library_user_id, author_id, is_blacklist, is_favorite) VALUES (1, 3, FALSE, TRUE);
+INSERT INTO user_author (library_user_id, author_id, is_blacklist, is_favorite) VALUES (1, 6, TRUE, FALSE);
+INSERT INTO user_author (library_user_id, author_id, is_blacklist, is_favorite) VALUES (2, 3, FALSE, TRUE);
+INSERT INTO user_author (library_user_id, author_id, is_blacklist, is_favorite) VALUES (2, 4, TRUE, FALSE);
+INSERT INTO user_author (library_user_id, author_id, is_blacklist, is_favorite) VALUES (3, 5, FALSE, TRUE);
+INSERT INTO user_author (library_user_id, author_id, is_blacklist, is_favorite) VALUES (3, 6, TRUE, FALSE);
 /* create book title table */
 CREATE TABLE book_title
 (
@@ -48,39 +60,45 @@ CREATE TABLE book_title
 	FOREIGN KEY (author_id) REFERENCES author(id)
 );
 /* book title inserts */
-INSERT INTO book_title (author_id, title_of_book, is_owned, own_wish_list, read_wish_list) VALUES (4, 'A Noble Masquerade',FALSE, TRUE, TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned, own_wish_list, read_wish_list) VALUES (4, 'An Elegant Facade',FALSE, TRUE, TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned, own_wish_list, read_wish_list) VALUES (4, 'A Lady of Esteem',FALSE, TRUE, TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned) VALUES (5, 'Ranger''s Apprentice: The Battle of Hackham Heath', TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned) VALUES (1, 'Pride and Prejudice', TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned) VALUES (1, 'Sense and Sensibility', TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned, read_wish_list) VALUES (2, 'Treasured Grace', FALSE, TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned, read_wish_list) VALUES (2, 'In The Shadow of Denali', FALSE, TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned, read_wish_list) VALUES (2, 'When You are Near', FALSE, TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned, read_wish_list) VALUES (2, 'Treasured Grace', FALSE, TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned, read_wish_list) VALUES (3, 'Unspoken', FALSE, TRUE);
-INSERT INTO book_title (author_id, title_of_book, is_owned, read_wish_list) VALUES (3, 'Undetected', FALSE, TRUE);
+INSERT INTO book_title (author_id, title_of_book) VALUES (4, 'A Noble Masquerade');
+INSERT INTO book_title (author_id, title_of_book) VALUES (4, 'An Elegant Facade');
+INSERT INTO book_title (author_id, title_of_book) VALUES (4, 'A Lady of Esteem');
+INSERT INTO book_title (author_id, title_of_book) VALUES (5, 'Ranger''s Apprentice: The Battle of Hackham Heath');
+INSERT INTO book_title (author_id, title_of_book) VALUES (1, 'Pride and Prejudice');
+INSERT INTO book_title (author_id, title_of_book) VALUES (1, 'Sense and Sensibility');
+INSERT INTO book_title (author_id, title_of_book) VALUES (2, 'Treasured Grace');
+INSERT INTO book_title (author_id, title_of_book) VALUES (2, 'In The Shadow of Denali');
+INSERT INTO book_title (author_id, title_of_book) VALUES (2, 'When You are Near');
+INSERT INTO book_title (author_id, title_of_book) VALUES (2, 'Treasured Grace');
+INSERT INTO book_title (author_id, title_of_book) VALUES (3, 'Unspoken');
+INSERT INTO book_title (author_id, title_of_book) VALUES (3, 'Undetected');
 /* select author names by author by id */
 SELECT author.first_name, author.middle_name, author.last_name, book_title.title_of_book FROM author INNER JOIN book_title ON author.id = book_title.author_id;
-/* select books by owned wish list */
-SELECT * FROM book_title WHERE own_wish_list = TRUE;
-/* select books by read wish list */
-SELECT * FROM book_title WHERE read_wish_list = TRUE;
 /* create book owned table */
 CREATE TABLE user_book
 (
 	id serial,
 	library_user_id INT NOT NULL,
-	author_id INT NOT NULL,
 	book_title_id INT NOT NULL,
 	is_owned BOOLEAN NOT NULL,
 	own_wish_list BOOLEAN,
 	read_wish_list BOOLEAN,
 	PRIMARY KEY (id),
 	FOREIGN KEY (library_user_id) REFERENCES library_user(id),
-	FOREIGN KEY (author_id) REFERENCES author(id),
 	FOREIGN KEY (book_title_id) REFERENCES book_title(id)	
 );
+INSERT INTO book_title (library_user_id, title_of_book, is_owned, own_wish_list, read_wish_list) VALUES (1, 'A Noble Masquerade', FALSE, TRUE, TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned, own_wish_list, read_wish_list) VALUES (1, 'An Elegant Facade', FALSE, TRUE, TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned, own_wish_list, read_wish_list) VALUES (1, 'A Lady of Esteem', FALSE, TRUE, TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned) VALUES (1, 'Ranger''s Apprentice: The Battle of Hackham Heath', TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned) VALUES (1, 'Pride and Prejudice', TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned) VALUES (1, 'Sense and Sensibility', TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned, read_wish_list) VALUES (2, 'Treasured Grace', FALSE, TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned, read_wish_list) VALUES (2, 'In The Shadow of Denali', FALSE, TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned, read_wish_list) VALUES (2, 'When You are Near', FALSE, TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned, read_wish_list) VALUES (2, 'Treasured Grace', FALSE, TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned, read_wish_list) VALUES (3, 'Unspoken', FALSE, TRUE);
+INSERT INTO book_title (library_user_id, title_of_book, is_owned, read_wish_list) VALUES (3, 'Undetected', FALSE, TRUE);
 /* create reviews table */
 CREATE TABLE reviews
 (
@@ -90,12 +108,13 @@ CREATE TABLE reviews
 	review TEXT NOT NULL,
 	rating INT,
 	PRIMARY KEY (id),
-	FOREIGN KEY (book_title_id) REFERENCES book_title(id)
+    FOREIGN KEY (library_user_id) REFERENCES library_user(id),
 	FOREIGN KEY (book_title_id) REFERENCES book_title(id)
 );
 /* insert reviews */
-INSERT INTO reviews (book_title_id, review, rating) VALUES (1, 'Loved the mystery and humor involved in the historical romance', 5);
-
+INSERT INTO reviews (library_user_id, book_title_id, review, rating) VALUES (1, 1, 'Loved the mystery and humor involved in the historical romance', 5);
+INSERT INTO reviews (library_user_id, book_title_id, review, rating) VALUES (2, 1, 'Deathly boring.', 5);
+INSERT INTO reviews (library_user_id, book_title_id, review, rating) VALUES (3, 2, 'Great read for my wife. Not so much for me.', 5);
 /* create borrowers */
 CREATE TABLE borrower
 (
@@ -125,5 +144,10 @@ CREATE TABLE loan
 	FOREIGN KEY (borrower_id) REFERENCES borrower(id)
 );
 /* insert loans */
-INSERT INTO loan (book_title_id, borrower_id, date_borrowed, is_returned) VALUES (4, 2, DATE '01-23-2021', FALSE);
-INSERT INTO loan (book_title_id, borrower_id, date_borrowed, is_returned) VALUES (3, 1, DATE '01-22-2021', FALSE);
+INSERT INTO loan (library_user_id, book_title_id, borrower_id, date_borrowed, is_returned) VALUES (1, 4, 2, DATE '01-23-2021', FALSE);
+INSERT INTO loan (library_user_id, book_title_id, borrower_id, date_borrowed, is_returned) VALUES (1, 3, 1, DATE '01-22-2021', FALSE);
+INSERT INTO loan (library_user_id, book_title_id, borrower_id, date_borrowed, return_date, is_returned) VALUES (1, 2, 1, DATE '01-02-2021', DATE '01-20-2021', FALSE);
+INSERT INTO loan (library_user_id, book_title_id, borrower_id, date_borrowed, is_returned) VALUES (2, 3, 2, DATE '01-22-2021', FALSE);
+INSERT INTO loan (library_user_id, book_title_id, borrower_id, date_borrowed, is_returned) VALUES (2, 5, 3, DATE '01-22-2021', FALSE);
+INSERT INTO loan (library_user_id, book_title_id, borrower_id, date_borrowed, is_returned) VALUES (3, 6, 1, DATE '01-22-2021', FALSE);
+INSERT INTO loan (library_user_id, book_title_id, borrower_id, date_borrowed, is_returned) VALUES (3, 3, 2, DATE '01-22-2021', FALSE);
