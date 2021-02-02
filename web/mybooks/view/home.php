b79cdd01f37e8a3bc3e74911e888aba55d23c345
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 19:58:45 -0700

    home page

--- web/mybooks/view/home.php
+++ web/mybooks/view/home.php
@@ -25,16 +25,7 @@
         </div>
         <div class="row d-flex justify-content-around">
             <div id="box-1" class="main-box bg-danger shadow-lg rounded box p-5 w-25 mt-4 mx-2">
-                <form method="post" action="index.php">
-                    <input class="rounded border-light m-1" type="text" name="username" placeholder="username"><br>
-                    <input class="rounded border-light m-1" type='text' name="password" placeholder="password"><br>
-                    <input type="submit" class="btn btn-custom bg-orange m-1" value="Log In">
-                </form>
-            </div>
-            <div id="box-1" class="main-box bg-danger shadow-lg rounded box p-5 w-25 mt-4 mx-2">
                 <h2 class="text-center p-3"><a href="index.php?action=catalog" title="Catalog">Catalog</a></h2>
-            </div>
-            <h2 class="text-center p-3"><a href="index.php?action=catalog" title="Catalog">Catalog</a></h2>
             </div>
             <div id="box-2" class="main-box bg-info shadow-lg rounded box p-5 w-25 mt-4 mx-2">
                 <h2 class="text-center p-3"><a href="index.php?action=wish" title="Book Wish List">Wish List</a></h2>
