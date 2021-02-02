cdfb851f981faa64c7d7c74a61eeaef5279c3341
Breanna Hansen <hansbrea@hotmail.com>
2021-02-02 13:44:02 -0700

    loan list

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -133,13 +133,11 @@ function displayAuthors($authors) {
 function displayLoans($loans) {
    $loanList = '<tbody>';
    foreach ($loans as $loan) {
-      $title = getDetails($loan['book_title_id']);
-      //$borrower = getBorrower($loan['borrower_id']);
-      $loanList .= '<tr><td>' . $title[0]['title_of_book'] . '</td>';
-      //$loanList .= '<td>' . $borrower[0]['first_name'] . ' ' . $borrower[0]['last_name'] . '</td>';
+      $loanList .= '<tr><td>' . $loan['title_of_book'] . '</td>';
+      $loanList .= '<td>' . $loan['first_name'] . ' ' . $loan['last_name'] . '</td>';
       $loanList .= '<td>' . $loan['date_borrowed'] . '</td>';
       $loanList .= '<td>' . $loan['return_date'] . '</td>'; 
-      $loanList .= '<td>' . $loan['is_returned'] . '</td>';
+      $loanList .= '<td><a class="btn btn-small bg-orange" href="sms:' . $loan['phone'] . '">SMS</a></td>';
       // if($loan['is_favorite'] == 't') {
       //    $loanList .= '<td class="text-center">Yes</td>';
       // } else {
