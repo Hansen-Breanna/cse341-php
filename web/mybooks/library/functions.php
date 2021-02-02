932ffdd6e1083cb921b4a621feeaaee7cc20d351
Breanna Hansen <hansbrea@hotmail.com>
2021-02-01 21:02:41 -0700

    home page

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -201,7 +201,7 @@ function getUserID($username, $password) {
       echo "An error occurred.\n";
       exit;
    } else {
-      $stmt = $db->query('SELECT * FROM library_user');
+      $stmt = $db->query('SELECT id FROM library_user WHERE username = :username');
       //$stmt->execute(array(':username' => $username));
       //$stmt->execute(array(':user_password' => $password));
       $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
