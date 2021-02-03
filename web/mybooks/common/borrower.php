71f35b5fba6cb0665d852222ded4b01c21515f0d
Breanna Hansen <hansbrea@hotmail.com>
2021-02-03 16:27:00 -0700

    add borrower include

---
+++ web/mybooks/common/borrower.php
@@ -0,0 +1,9 @@
+<div class="p-2">
+        <label class="ml-1">Search by Borrower:</label><br>
+        <div class="d-flex flex-wrap">
+            <input class="rounded p-1 m-1" type="text" name="first-name" id="first-name" placeholder="First Name">
+            <input class="rounded p-1 m-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
+            <input type="submit" class="btn btn-custom bg-info m-1">
+        </div>
+    </div>
+</form>
