dd2d8bb926dd3cdf68eed69c50e76a840366ebf9
Breanna Hansen <hansbrea@hotmail.com>
2021-02-03 14:24:13 -0700

    searches

--- web/mybooks/view/authors.php
+++ web/mybooks/view/authors.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && (!isset($_SESSION))) {
   <div class="row d-flex flex-column align-items-center">
     <div>
       <!-- Search Author -->
-      <form method="post" action="index.php?action=catalog">
+      <form method="post" action="index.php?action=authors">
       <?php include 'common/author.php'; ?>
     </div>
     <div>
