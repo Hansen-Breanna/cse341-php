b1a2ddf38a7a2f24c445b95f4867bb05377f3ca4
Breanna Hansen <hansbrea@hotmail.com>
2021-02-03 14:57:56 -0700

    searches

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -76,14 +76,4 @@ function getAuthors($id) {
    return $results;
 }
 
-// search by title
-function getTitle($title) {
-   $db = connectMyBooks();
-   $stmt = $db->prepare('SELECT a.first_name, a.middle_name, a.last_name, u.is_blacklist, u.is_favorite FROM author a INNER JOIN user_author u ON a.id = u.author_id WHERE u.library_user_id = :id ORDER BY last_name');
-   $stmt->bindValue(':title', $title, PDO::PARAM_INT);
-   $stmt->execute();
-   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-   return $results;
-}
-
 ?>
