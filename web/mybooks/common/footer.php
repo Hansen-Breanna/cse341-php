91e2669cc0367e2bb58cf97d612273c6085266bb
Breanna Hansen <hansbrea@hotmail.com>
2021-02-03 12:30:43 -0700

    message

--- web/mybooks/common/footer.php
+++ web/mybooks/common/footer.php
@@ -1,12 +1,12 @@
 </div>
 
 <footer class="bg-info mt-3 p-3 border-top">
-    <div class="container d-flex justify-content-between align-items-center">
+    <div class="container d-flex justify-content-between align-items-start">
         <div class="row">
             <p class="mb-0">&copy;&nbsp; <?php echo date("Y") ;?> - Breanna Hansen</p>
         </div>
         <div>
-            <form method='post' action="index.php#logout">
+            <form method='post' action="index.php">
                 <input type='hidden' id="logout" name="logout" value="logout"><br>
                 <input type="submit" class="btn bg-orange m-1 p-2 shadow" value="Log Out">
             </form>
