91fe10474ace4a9aa2cb9abe77cf4f9d1b57ec89
Breanna Hansen <hansbrea@hotmail.com>
2021-02-02 22:07:15 -0700

    loan list

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -138,7 +138,7 @@ function displayLoans($loans) {
       $loanList .= '<tr><td>' . $loan['title_of_book'] . '</td>';
       $loanList .= '<td>' . $loan['first_name'] . ' ' . $loan['last_name'] . '</td>';
       $loanList .= '<td>' . $loan['date_borrowed'] . '</td>';
-      $loanList .= '<td>' . $loan['return_date'] . '</td>'; 
+      $loanList .= '<td>' . overdue($loan['return_date']) . '</td>'; 
       $loanList .= '<td><a class="btn btn-small bg-orange" href="sms://+1' . $loan['phone_number'];
       $loanList .= '?&body=Hi%20' . $loan['first_name'] . '!%20The%20book%20you%20borrowed%20' . $loan['title_of_book'];
       $loanList .= '%20is%20overdue.%20Please%20contact%20me%20at%20' . $loan['user_phone'];
@@ -146,6 +146,12 @@ function displayLoans($loans) {
    }
    $loanList .= '</tbody>';
    return $loanList;
+}
+
+function overdue($date) {
+   if($date > date("Y-m-d")) {
+      echo "Yes";
+   }
 }
 
 function displayReviews($reviews) {
