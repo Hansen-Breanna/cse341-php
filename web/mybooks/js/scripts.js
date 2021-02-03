8796e273371ec9eed0e13d0ec2fee4979acede60
Breanna Hansen <hansbrea@hotmail.com>
2021-02-03 12:29:18 -0700

    message

--- web/mybooks/js/scripts.js
+++ web/mybooks/js/scripts.js
@@ -1,11 +1,12 @@
 // Toggle visibility of week assignments
 // https://api.jquery.com/slidetoggle/
 $(document).ready(function(){
-      // Nav toggle
+      
+    $("#logout").hide();
+    
+    // Nav toggle
       $(".navbar-toggler").click(function(){
         $("#link-nav").toggle();
       });
-
-      $("#logout").hide();
   });
 
