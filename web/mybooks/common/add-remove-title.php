2d12a1dd83cc42d0218b4b82b617c344e32e4171
Breanna Hansen <hansbrea@hotmail.com>
2021-02-04 07:48:00 -0700

    message

--- web/mybooks/common/add-remove-title.php
+++ web/mybooks/common/add-remove-title.php
@@ -1,4 +1,4 @@
-<div class="m-3 py-2 border-secondary border-top border-bottom">
+<div class="my-3 mx-2 py-2 border-secondary border-top border-bottom">
     <a href="index.php?action=add-book" class="btn btn-custom bg-orange text-dark my-2">Add Title</a>
     <a href="index.php?action=remove-book" class="btn btn-custom bg-orange text-dark my-2">Remove Title</a>
 </div>
