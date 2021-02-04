740f7afdc0948807626392518d1832fd927540ff
Breanna Hansen <hansbrea@hotmail.com>
2021-02-03 22:46:36 -0700

    message

--- web/mybooks/common/author.php
+++ web/mybooks/common/author.php
@@ -1,8 +1,8 @@
     <div class="p-2">
         <label class="ml-1">Search by Author:</label><br>
         <div class="d-flex flex-wrap">
-            <input class="rounded p-1 m-1" type="text" name="first-name" id="first-name" placeholder="First Name">
-            <input class="rounded p-1 m-1" type="text" name="last-name" id="last-name" placeholder="Last Name">
+            <input class="rounded p-1 m-1" type="text" name="first_name" id="first_name" placeholder="First Name">
+            <input class="rounded p-1 m-1" type="text" name="last_name" id="last_name" placeholder="Last Name">
             <input type="submit" class="btn btn-custom bg-info m-1">
         </div>
     </div>
