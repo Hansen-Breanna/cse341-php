8e93e9ed8f898c29339eccb238fdaef9ac433adb
Breanna Hansen <hansbrea@hotmail.com>
2021-02-03 17:06:23 -0700

    message

--- web/mybooks/view/catalog.php
+++ web/mybooks/view/catalog.php
@@ -15,6 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $_SESSION['first_name'] = $first_name;
   $_SESSION['last_name'] = $last_name;
   $retrieveTitle = getTitle($_SESSION['title'], $_SESSION['id']);
+  $displayTitle = displayCatalog($retrieveTitle);
 }
 
 ?>
