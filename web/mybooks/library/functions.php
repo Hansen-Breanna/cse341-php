2f0c0af719ea197c08aa2aa521a74f9f3a07d791
Breanna Hansen <hansbrea@hotmail.com>
2021-02-03 23:44:06 -0700

    message

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -58,7 +58,7 @@ function getCatalog($id) {
    $stmt = $db->prepare('SELECT b.title_of_book, bo.first_name, bo.last_name, bo.phone_number, l.date_borrowed, l.return_date, 
    l.is_returned, lu.user_phone FROM loan l INNER JOIN book_title b ON l.book_title_id = b.id INNER JOIN library_user lu 
    ON lu.id = l.library_user_id INNER JOIN borrower bo ON l.borrower_id = bo.id WHERE l.library_user_id = :id 
-   AND a.first_name = :first_name AND a.last_name = :last_name');
+   AND bo.first_name = :first_name AND bo.last_name = :last_name');
    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
    $stmt->bindValue(':first_name', $first_name, PDO::PARAM_STR);
    $stmt->bindValue(':last_name', $last_name, PDO::PARAM_STR);
