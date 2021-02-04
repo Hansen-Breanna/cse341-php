4fb855a94e4cb4a4adc6304cb577a78b0cc8a4f2
Breanna Hansen <hansbrea@hotmail.com>
2021-02-03 18:10:53 -0700

    message

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -78,11 +78,11 @@ function getReadWishes($id) {
 }
 
 // get read wish list title
-function getReadTitle($id) {
+function getReadTitle($title, $id) {
    $db = connectMyBooks();
    $stmt = $db->prepare('SELECT b.title_of_book, a.first_name, a.middle_name, a.last_name FROM user_book u INNER JOIN book_title b ON u.book_title_id = b.id INNER JOIN author a ON a.id = b.author_id WHERE u.library_user_id = :id AND read_wish_list = TRUE AND b.title_of_book = :title');
    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-   $stmt->bindValue(':id', $id, PDO::PARAM_STR);
+   $stmt->bindValue(':title', $title, PDO::PARAM_STR);
    $stmt->execute();
    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
    return $results;
