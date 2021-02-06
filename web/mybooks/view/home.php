281569c3f39446d04791bd2d9813257043ee90c7
Breanna Hansen <hansbrea@hotmail.com>
2021-02-06 13:48:23 -0700

    message

--- web/mybooks/view/home.php
+++ web/mybooks/view/home.php
@@ -54,19 +54,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <input type="submit" class="rs-size btn btn-custom bg-orange m-1" value="Log In">
                 </form>
             </div>
-            <div id="box-1" class="main-box bg-danger shadow-lg rounded box p-5 w-25 mt-4 mx-2">
+            <div id="box-2" class="main-box bg-danger shadow-lg rounded box p-5 w-25 mt-4 mx-2">
                 <h2 class="text-center p-3"><a href="index.php?action=catalog" title="Catalog">Catalog</a></h2>
             </div>
-            <div id="box-2" class="main-box bg-info shadow-lg rounded box p-5 w-25 mt-4 mx-2">
+            <div id="box-3" class="main-box bg-info shadow-lg rounded box p-5 w-25 mt-4 mx-2">
                 <h2 class="text-center p-3"><a href="index.php?action=wish" title="Book Wish List">Wish List</a></h2>
             </div>
-            <div id="box-3" class="main-box bg-success shadow-lg rounded box p-5 w-25 mt-4 mx-2">
+            <div id="box-4" class="main-box bg-success shadow-lg rounded box p-5 w-25 mt-4 mx-2">
                 <h2 class="text-center p-3"><a href="index.php?action=authors" title="Authors">Authors</a></h2>
             </div>
-            <div id="box-2" class="main-box bg-orange shadow-lg rounded box p-5 w-25 mt-4 mx-2">
+            <div id="box-5" class="main-box bg-orange shadow-lg rounded box p-5 w-25 mt-4 mx-2">
                 <h2 class="text-center p-3"><a href="index.php?action=loans" title="Loans">Loans</a></h2>
             </div>
-            <div id="box-3" class="main-box bg-warning shadow-lg rounded box p-5 w-25 mt-4 mx-2">
+            <div id="box-6" class="main-box bg-warning shadow-lg rounded box p-5 w-25 mt-4 mx-2">
                 <h2 class="text-center p-3"><a href="index.php?action=reviews" title="Book Reviews">Reviews</a></h2>
             </div>
         </div>
