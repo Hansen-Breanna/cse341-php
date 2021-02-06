8fdfd356e54b34be61b5f6ba6cd1a1778696f08f
Breanna Hansen <hansbrea@hotmail.com>
2021-02-06 11:20:11 -0700

    message

--- web/mybooks/view/home.php
+++ web/mybooks/view/home.php
@@ -5,8 +5,10 @@ session_start ();
 // define variables and set to empty values
 $username = $password = $logout = "";
 
+$message = "<p class='bg-success p-2 d-block'>You are logged in as " . $_SESSION['username'] . '</p>';
+
 if ($_SERVER["REQUEST_METHOD"] == "GET" && (isset($_SESSION['id'])))  {
-    $message = "<p class='bg-success p-2'>You are logged in as " . $_SESSION['username'] . '</p>';
+    $message;
 }
  
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -19,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         session_destroy();
     }
     if (isset($_SESSION['id'])) {
-        $message = "<p class='bg-success py-3 px-4 rounded ml-3'>You are logged in as " . $_SESSION['username'] . '</p>';
+        $message;
     }   
 }
 
