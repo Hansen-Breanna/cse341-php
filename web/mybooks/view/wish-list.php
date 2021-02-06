39186a22940f900fe9c89b2a89a1717b07a8094d
Breanna Hansen <hansbrea@hotmail.com>
2021-02-06 13:24:56 -0700

    message

--- web/mybooks/view/wish-list.php
+++ web/mybooks/view/wish-list.php
@@ -1,9 +1,8 @@
 <?php
 // start session
-session_start ();
+session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "GET" && (!isset($_SESSION))) { 
- 
+if ($_SERVER["REQUEST_METHOD"] == "GET" && (!isset($_SESSION))) {
 } else {
 }
 
@@ -31,55 +30,55 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && (!isset($_SESSION))) {
       <div>
         <!-- Search Title -->
         <form method="post" action="index.php?action=wish-title">
-        <?php include 'common/title.php'; ?>
+          <?php include 'common/title.php'; ?>
 
-        <!-- Search Author -->
-        <form method="post" action="index.php?action=wish-author">
-        <?php include 'common/author.php'; ?>
+          <!-- Search Author -->
+          <form method="post" action="index.php?action=wish-author">
+            <?php include 'common/author.php'; ?>
 
-        <!-- Add/Remove -->
-        <?php include 'common/add-remove-title.php'; ?>
-        <a href="index.php?action=wish" title="See All Wishes" class="btn btn-custom bg-info my-2 ml-1">See All Wishes</a>
-        </div>
+            <!-- Add/Remove -->
+            <?php include 'common/add-remove-title.php'; ?>
+            <a href="index.php?action=wish" title="See All Wishes" class="btn btn-custom bg-info my-2 ml-1">See All Wishes</a>
       </div>
-      <div>
-          <?php 
-            if ($_SERVER["REQUEST_METHOD"] == "GET") {
-              echo $message;
-            }
-          ?>
-        <div class="d-flex justify-content-around">
-          <div class="m-2">
-            <h2 class="pt-4">Own Wish List</h2>
-            <table class="table table-dark table-striped text-light m-2">
-              <thead>
-                <tr>
-                  <th class="pl-1">Book Title</th>
-                  <th class="pl-5">Author</th>
-                </tr>
-              </thead>
-              <?php
-              echo '<div>' . $ownTable . '</div>';
-              ?>
-            </table>
-          </div>
-          <div class="m-2">
-            <h2 class="pt-4">Read Wish List</h2>
-            <table class="table table-dark table-striped text-light m-2">
-              <thead>
-                <tr>
-                  <th class="pl-1">Book Title</th>
-                  <th class="pl-5">Author</th>
-                </tr>
-              </thead>
-              <?php
-              echo '<div' . $readTable . '</div>';
-              ?>
-            </table>
-          </div>
+    </div>
+    <div>
+      <?php
+      if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        echo $message;
+      }
+      ?>
+      <div class="d-flex flex-row flex-wrap justify-content-around">
+        <div class="m-2">
+          <h2 class="pt-4">Own Wish List</h2>
+          <table class="table table-dark table-striped text-light m-2">
+            <thead>
+              <tr>
+                <th class="pl-1">Book Title</th>
+                <th class="pl-5">Author</th>
+              </tr>
+            </thead>
+            <?php
+            echo '<div>' . $ownTable . '</div>';
+            ?>
+          </table>
+        </div>
+        <div class="m-2">
+          <h2 class="pt-4">Read Wish List</h2>
+          <table class="table table-dark table-striped text-light m-2">
+            <thead>
+              <tr>
+                <th class="pl-1">Book Title</th>
+                <th class="pl-5">Author</th>
+              </tr>
+            </thead>
+            <?php
+            echo '<div' . $readTable . '</div>';
+            ?>
+          </table>
         </div>
       </div>
     </div>
+  </div>
   </div>
 </main>
 
