0eeba45330b71f37434655923df2da2a6973479c
Breanna Hansen <hansbrea@hotmail.com>
2021-02-06 12:46:48 -0700

    message

--- web/mybooks/view/reviews.php
+++ web/mybooks/view/reviews.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && (!isset($_SESSION))) {
             <div class="my-3 mx-2 py-2 border-secondary border-top border-bottom">
               <a href="index.php?action=add-book" class="btn btn-custom bg-orange my-2 ml-1">Add Review</a>
               <a href="index.php?action=remove-book" class="btn btn-custom bg-orange my-2 ml-1">Remove Review</a>
-              <a href="index.php?action=catalog" title="See All Reviews" class="btn btn-custom bg-info my-2 ml-1">See All Reviews</a>
+              <a href="index.php?action=reviews" title="See All Reviews" class="btn btn-custom bg-info my-2 ml-1">See All Reviews</a>
             </div>
       </div>
       <?php 
