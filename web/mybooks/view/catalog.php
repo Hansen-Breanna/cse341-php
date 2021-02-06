96ff748fd64173bcc2e73c3f79748fdfe3828931
Breanna Hansen <hansbrea@hotmail.com>
2021-02-06 12:19:56 -0700

    message

--- web/mybooks/view/catalog.php
+++ web/mybooks/view/catalog.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <!-- Add/Remove -->
             <?php include 'common/add-remove-title.php'; ?>
-            <a href="index.php?action=catalog" title="See All Titles" class="btn btn-custom bg-info m-2">See All Titles</a>
+            <a href="index.php?action=catalog" title="See All Titles" class="btn btn-custom bg-info my-2 ml-1">See All Titles</a>
             </div>
           </div>
           <div>
