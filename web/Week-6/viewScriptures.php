2dabc639286a8b7a1b6f8a2caad0d33765ff5521
Breanna Hansen <hansbrea@hotmail.com>
2021-02-08 23:19:13 -0700

    team activity

--- web/Week-6/viewScriptures.php
+++ web/Week-6/viewScriptures.php
@@ -22,19 +22,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     echo $book . ' ' . $chapter . ':' . $verse . ' - ' . $content;
 
-    $insert = insertScripture($db, $book, $chapter, $verse, $content);
-
-    function insertScripture($db, $book, $chapter, $verse, $content) {
         $stmt= $db->prepare('INSERT INTO scripture (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
-        $stmt->bindValue(':book', $book, PDO::PARAM_STR);
-        $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
-        $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
-        $stmt->bindValue(':content', $content, PDO::PARAM_STR);
-        $stmt->execute();
+        // $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+        // $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+        // $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
+        // $stmt->bindValue(':content', $content, PDO::PARAM_STR);
+        // $stmt->execute();
         $stmt->execute(array(':book' => $book, ':chapter' => $chapter, ':verse' => $verse, ':content' => $content));
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $results;
-    }
 
     $newScriptureID = $pdo->lastInsertId('scripture_id_sq');
     echo $newScriptureID;
