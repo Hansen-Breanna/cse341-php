f41ba3ed2b73fe6034162b60951d7cc403e2be40
Breanna Hansen <hansbrea@hotmail.com>
2021-02-08 17:18:52 -0700

    add/update/delete

--- web/mybooks/model/models.php
+++ web/mybooks/model/models.php
@@ -6,7 +6,8 @@ function displayCatalog($catalog) {
     foreach ($catalog as $book) {
        $bookList .= '<tr><td>' . $book['title_of_book'] . '</td>';
        $bookList .= '<td class="pl-5">' . $book['first_name'] . ' ' . $book['middle_name'] . ' ' . $book['last_name'] . '</td>';
-       $bookList .= '<td class="pl-5><a href="" class="bg-orange p-1" title="Edit"><i class="far fa-edit bg-orange p-2" data-toggle="tooltip" data-placement="top" title="Edit"></i></a>';
+       $bookList .= '<td class="pl-5><a href="" class="bg-orange p-1" title="Edit"><i class="far fa-edit bg-info p-2" data-toggle="tooltip" data-placement="top" title="Edit"></i></a>';
+       $bookList .= '<a href="" class="bg-orange p-1" title="Delete"><i class="far fa-trash bg-danger p-2" data-toggle="tooltip" data-placement="top" title="Delete"></i></a></td>';
        $bookList .= '</tr>';
     }
     $bookList .= '</tbody>';
