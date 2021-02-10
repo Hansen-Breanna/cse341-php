c8a21a2208d27b2911a8f3f829cf3e23c7c51884
Breanna Hansen <hansbrea@hotmail.com>
2021-02-10 13:39:51 -0700

    add title page

--- web/mybooks/view/add-title.php
+++ web/mybooks/view/add-title.php
@@ -24,11 +24,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     //user_author
     //insertUserAuthor($db, $_SESSION['id'], $newAuthorID, $blacklist, $favorite);
-    
+
     //book
     insertTitle($db, $newAuthorID, $title);
     $newTitleID = $db->lastInsertId('book_title_id_seq');
-    
+
     // //user-book
     insertUserBook($db, $_SESSION['id'], $newTitleID, $own, $own_wish, $read_wish);
 }
@@ -67,15 +67,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <form method="post" action="">
                     <div>
                         <!-- Author -->
-                        <p class="author-name">Author name</p>
+                        <h2 class="author-name">Author</h2>
                         <div class="author">
                             <label for="first-name">First name:</label>
                             <input type="text" name="first_name" id="first-name"><br>
-                        <!-- </div>
+                            <!-- </div>
                         <div class="middle-name"> -->
                             <label for="chapter">Middle Name:</label>
                             <input type="text" name="middle_name" id="middle-name"><br>
-                        <!-- </div>
+                            <!-- </div>
                         <div class="last-name"> -->
                             <label for="verse">Last name:</label>
                             <input type="text" name="last_name" id="last-name"><br>
@@ -86,41 +86,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         </div>
                     </div>
                     <!-- Title -->
-                    <div class="title">
-                        <label>Book title:</label>
-                        <input type="text" id="title" name="title">
+                    <div class="book-title">
+                        <h2>Title</h2>
+                        <div class="title">
+                            <label>Book title:</label>
+                            <input type="text" id="title" name="title">
+                        </div>
+                        <div class="user-title">
+                            <input type="checkbox" id="own" name="own" value="TRUE">Currently Own<br>
+                            <input type="checkbox" id="ownWish" name="own_wish" value="TRUE">Own Wish List<br>
+                            <input type="checkbox" id="readWish" name="read_wish" value="TRUE">Read Wish List<br>
+                        </div>
                     </div>
-                    <div class="user-title">
-                        <!-- library_user_id
-          book_title_id
-          owned
-          own Wish
-          read wish -->
-                        <input type="checkbox" id="own" name="own" value="TRUE">Currently Own<br>
-                        <input type="checkbox" id="ownWish" name="own_wish" value="TRUE">Own Wish List<br>
-                        <input type="checkbox" id="readWish" name="read_wish" value="TRUE">Read Wish List<br>
-                    </div>
-
-                    <!-- author
-          first, middle, last
-
-          user-Author
-          library_user_id
-          author_id
-          Blacklisted
-          Favorite
-          
-          book
-          --title
-          author id
-
-          user-book
-          library_user_id
-          book_title_id
-          owned
-          own Wish
-          read wish -->
-
                     <div class="submit">
                         <input type="submit">
                     </div>
