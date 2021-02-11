b216e0043bb00a089df67391f1d61109ea3d8607
Breanna Hansen <hansbrea@hotmail.com>
2021-02-10 20:16:59 -0700

    add title page

--- web/mybooks/view/add-title.php
+++ web/mybooks/view/add-title.php
@@ -104,6 +104,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <!-- Author -->
                         <h2 class="author-name mt-2">Author</h2>
                         <p>Select a current author or add a new author.</p>
+                        <p>
+                            <button type="button" class="btn bg-orange p-1" onclick="enableAddAuthor()">Add New Author</button>
+                            <button type="button" class="btn bg-orange p-1" onclick="enableSelectAuthor()">Use Existing Author</button>
+                        </p>
                         <table>
                             <tbody>
                                 <tr>
