a51cf5bdb39e60dbdb423b23486c836daa5098c2
Breanna Hansen <hansbrea@hotmail.com>
2021-02-11 21:23:26 -0700

    add form to author page

--- web/mybooks/view/authors.php
+++ web/mybooks/view/authors.php
@@ -10,8 +10,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $last_name = test_input($_POST["last_name"]);
 
   if (isset($_POST['delete'])) {
+    echo $_POST['delete'];
+    try {
     $deletID = test_input($_POST['delete']);
     deleteUserAuthor($deleteID);
+    } catch (Exception $e) {
+      $message = "<p class='px-4 py-3 bg-danger rounded'>Delete failed.</p>";
+    }
   } else if (isset($_POST['update'])) {
   } else {
     try {
