712b297b6053051f7c6dcb09b445474286746636
Breanna Hansen <hansbrea@hotmail.com>
2021-02-11 21:51:08 -0700

    add form to author page

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -243,6 +243,7 @@ function deleteUserAuthor($id) {
    echo "hello";
    $db = $db = connectMyBooks();
    $stmt = $db->prepare('DELETE FROM user_author WHERE author_id = :id');
+   echo "book";
    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
    $stmt->execute();
 }
