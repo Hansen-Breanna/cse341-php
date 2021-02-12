e2c677d252692791d887151831d777f1f1a95f32
Breanna Hansen <hansbrea@hotmail.com>
2021-02-12 10:29:36 -0700

    add review form

--- web/mybooks/view/add-review.php
+++ web/mybooks/view/add-review.php
@@ -133,11 +133,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
                     <h2 class="mt-2">Review</h2>
                     <p>Add review content and rating of 1-5, with 5 being the best.</p>
-                    <div class="d-flex justify-content-start mb-2">
+                    <div class="d-flex justify-content-start pb-2">
                         <label class="mr-1">Content:</label>
-                        <textarea class="rounded" col="100" rows="4" id="review-content" name="review"></textarea>
+                        <textarea class="rounded" col="140" rows="4" id="review-content" name="review"></textarea>
                     </div>
-                    <div class="mb-1">
+                    <div class="pb-1">
                         <label class="mr-1">Rating:</label>
                         <select name="rating" id="rating">
                             <option value="1">1</option>
