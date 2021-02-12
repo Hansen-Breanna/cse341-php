82791a081d343d39cd526d2f76118a5ec25fe682
Breanna Hansen <hansbrea@hotmail.com>
2021-02-12 14:30:14 -0700

    start loan form

--- web/mybooks/view/loans.php
+++ web/mybooks/view/loans.php
@@ -33,7 +33,6 @@ session_start();
             <?php include 'common/borrower.php'; ?>
 
             <div class="my-3 mx-2 py-2 border-secondary border-top border-bottom">
-              <a href="index.php?action=add-loan" class="btn btn-custom bg-orange my-2 ml-1 ">Add Borrower</a>
               <a href="index.php?action=add-loan" class="btn btn-custom bg-orange my-2 ml-1 ">Add Loan</a>
               <a href="index.php?action=loans" title="See All Borrowers" class="btn btn-custom my-2 ml-1 bg-info">See All Borrowers</a>
             </div>
