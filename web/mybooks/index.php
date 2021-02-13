cd7cb44d0baea53641bd3ef84c860f12926e8c73
Breanna Hansen <hansbrea@hotmail.com>
2021-02-12 23:41:35 -0700

    message

--- web/mybooks/index.php
+++ web/mybooks/index.php
@@ -243,11 +243,11 @@ switch ($action){
         include 'view/add-review.php';
     break;
     case 'update-author':
-        $authors = getAuthors($_SESSION['id']);
-        $authorData = getAuthor($_SESSION['authorID']);
-        $authorsTable = displayAuthors($authors);
-        $author = displayAuthorData($authorData);
-        $choice = addFavBlack();
+        // $authors = getAuthors($_SESSION['id']);
+        // $authorData = getAuthor($_SESSION['authorID']);
+        // $authorsTable = displayAuthors($authors);
+        // $author = displayAuthorData($authorData);
+        // $choice = addFavBlack();
         //$message-2 = "<p class='px-4 py-3 bg-danger rounded'>Author already exists.</p>";
         include 'view/authors.php';
     break;
