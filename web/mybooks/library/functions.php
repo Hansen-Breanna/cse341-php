c52bba8066f8c1a669333fa2ecbf7c9b5d2bce52
Breanna Hansen <hansbrea@hotmail.com>
2021-02-12 20:48:07 -0700

    delete authors

--- web/mybooks/library/functions.php
+++ web/mybooks/library/functions.php
@@ -293,6 +293,7 @@ function deleteTitle($db, $id, $titleID) {
 
 // Get author id by using names
 function getAuthorID($db, $first_name, $middle_name, $last_name) {
+   echo $first_name;
    $stmt = $db->prepare('SELECT a.id FROM author WHERE first_name = :first_name AND middle_name = :middle_name AND last_name = :last_name');
    $stmt->execute(array(':first_name' => $first_name, ':middle_name' => $middle_name, ':last_name' => $last_name));
    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
