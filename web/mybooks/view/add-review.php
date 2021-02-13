ed1d2f51dfb94c3d1a59e06a4ca9fe61c6c17531
Breanna Hansen <hansbrea@hotmail.com>
2021-02-12 17:37:50 -0700

    start loan form

--- web/mybooks/view/add-review.php
+++ web/mybooks/view/add-review.php
@@ -84,7 +84,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <h2 class="mt-2">Title</h2>
                     <div class="book-title">
                         <label class="mr-1">Book title:</label>
-                        <?php $bookTitle ?>
+                        <?php echo $bookTitle; ?>
                     </div>
                     <h2 class="mt-2">Review</h2>
                     <p>Add review content and rating of 1-5, with 5 being the best.</p>
