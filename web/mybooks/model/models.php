5e4fd3ef7f11374311c7751741b91380c83f3370
Breanna Hansen <hansbrea@hotmail.com>
2021-02-12 21:59:11 -0700

    delete loan

--- web/mybooks/model/models.php
+++ web/mybooks/model/models.php
@@ -34,11 +34,11 @@ function displayLoans($loans) {
        $loanList .= '?&body=Hi%20' . $loan['first_name'] . '!%20The%20book%20you%20borrowed%2C%20' . $loan['title_of_book'];
        $loanList .= '%2C%20is%20overdue.%20Please%20contact%20me%20at%20' . $loan['user_phone'];
        $loanList .= '%20to%20return%20it.%20Thanks!">SMS</a>';
-       $loanList .= '<form method="post" action="index.php?action=authors">
+       $loanList .= '<form method="post" action="index.php?action=loans">
          <input type="hidden" name="update" value="' . $loan['id'] . '"/>
          <input type="submit" class="bg-info btn btn-small mr-1 mb-1" value="Edit"/>
        </form>
-       <form method="post" action="index.php?action=authors">
+       <form method="post" action="index.php?action=loans">
          <input type="hidden" name="delete" value="' . $loan['id'] . '"/>
          <input type="submit" class="bg-danger btn btn-small mr-1 mb-1" value="Delete"/>
       </form>
