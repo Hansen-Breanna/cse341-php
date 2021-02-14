b14aea1d39a7177ffa1c1b50fe9ff4694f91d30d
Breanna Hansen <hansbrea@hotmail.com>
2021-02-13 20:29:12 -0700

    update catalog

--- web/mybooks/index.php
+++ web/mybooks/index.php
@@ -114,8 +114,8 @@ switch ($action){
     case 'wish':
         $readWishes = getReadWishes($_SESSION['id']);
         $ownWishes = getOwnWishes($_SESSION['id']);
-        $readTable = displayCatalog($readWishes);
-        $ownTable = displayCatalog($ownWishes);
+        $readTable = displayWishList($readWishes);
+        $ownTable = displayWishList($ownWishes);
         $author = selectAuthor($db);
         include 'view/wish-list.php';
     break;
