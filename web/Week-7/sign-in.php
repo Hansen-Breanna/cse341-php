0d39f617a6ff90fc10cd63406cc13d84a20fe6d1
Breanna Hansen <hansbrea@hotmail.com>
2021-02-16 10:24:22 -0700

    testing

--- web/Week-7/sign-in.php
+++ web/Week-7/sign-in.php
@@ -15,6 +15,8 @@ require_once '../mybooks/library/connections.php';
         $data = check_id($db, $user, $pass);
         echo $data[0]['username'];
         echo $user;
+        echo $pass;
+        echo $data[0]['password'];
         foreach ($data as $user) {
             if ($user[0]['username'] == $user && $user[0]['user_password'] == $pass) {
                 $hash = $user[0]['user_password'];
