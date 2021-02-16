050e278d034a44fec9ad5ad165fb5c4fe4ffe18b
Breanna Hansen <hansbrea@hotmail.com>
2021-02-15 23:18:35 -0700

    week 7 activity

--- web/Week-7/sign-up.php
+++ web/Week-7/sign-up.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $db = connectMyBooks();
         $stmt = $db->prepare('INSERT INTO week7_user (username, user_password) VALUES (:user, :pass)');
         $stmt->execute(array(':user' => $user, ':pass' => $pass));
-        header('Location: sign-in.php');
+        header('Location: signin.php');
     } catch (Exception $e) {
         echo $e;
     }
