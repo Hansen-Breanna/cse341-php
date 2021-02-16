9320a51dcc2eabae72eae969fac83129f8348477
Breanna Hansen <hansbrea@hotmail.com>
2021-02-15 23:19:22 -0700

    test page

---
+++ web/Week-7/signin.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Week 07 Teach: Team Activity</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+    <h1>Sign In</h1>
+    <p>Please sign in.</p>
+        <form method="post" action="welcome.php">
+            <input class="m-1 pl-1" type="text" id="username" name="username" placeholder="username"><br>
+            <input class="m-1 pl-1" type='password' id="password" name="password" placeholder="password"><br>
+            <input type="submit" class="btn bg-primary m-1" value="Log In">
+        </form>
+        <script src="" async defer></script>
+    </body>
+</html>
