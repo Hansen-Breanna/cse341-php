38c89956ebfa4fec06e24d13c4a9cea0d4121a26
Breanna Hansen <hansbrea@hotmail.com>
2021-02-16 11:00:49 -0700

    team activity

--- web/Week-7/sign-in.php
+++ web/Week-7/sign-in.php
@@ -10,10 +10,9 @@ require_once '../mybooks/library/connections.php';
  {
      $user = test_input($_POST['username']);
      $pass = test_input($_POST['password']);
- 
+    echo $user . $pass;
      try {
-        $hash = "";
-        $data = check_id($db, $user, $pass);
+        $data = check_id($db, $user);
         var_dump($data);
             $verify = password_verify($pass, $data[0]['user_password']);
             echo $user_data['username'];
@@ -31,11 +30,10 @@ require_once '../mybooks/library/connections.php';
      }
  }
 
- function check_id($db, $user, $pass) 
+ function check_id($db, $user) 
  {
     $stmt = $db->prepare('SELECT username, user_password FROM week7_user WHERE username = :user');
     $stmt->bindValue(':user', $user, PDO::PARAM_STR);
-    $stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
     $stmt->execute();
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     var_dump($results);
