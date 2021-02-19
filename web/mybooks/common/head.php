172893a75da0b1b67dcfd7a32562f4edc094adb6
Breanna Hansen <hansbrea@hotmail.com>
2021-02-19 11:18:16 -0700

    add favicon

--- web/mybooks/common/head.php
+++ web/mybooks/common/head.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link rel="shortcut icon" type="image/jpg" href="../images/favicon-16x16.png"/>
+    <link rel="icon" type="image/x-icon" href="../images/favicon-16x16.png"/>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
