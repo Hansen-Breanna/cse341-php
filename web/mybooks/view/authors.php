c6973dce3a92a06e75478655f110dc6d8e023bff
Breanna Hansen <hansbrea@hotmail.com>
2021-02-20 16:58:14 -0700

    add user author

--- web/mybooks/view/authors.php
+++ web/mybooks/view/authors.php
@@ -116,11 +116,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <form method="post" action="index.php?action=get-author">
           <input type="hidden" name="search">
           <?php include 'common/author.php'; ?>
+          <a href="index.php?action=authors" title="See All Authors" class="btn btn-custom my-2 ml-1 bg-info">See All Authors</a>
       </div>
-
-      <div class="my-3 mx-2 py-2 border-secondary border-top border-bottom">
-                <a href="index.php?action=authors" title="See All Authors" class="btn btn-custom my-2 ml-1 bg-info">See All Authors</a>
-            </div>
       <div>
         <?php
         echo $message;
